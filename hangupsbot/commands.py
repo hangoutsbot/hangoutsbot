433b6a06cc081dea6bf5ff87b9d8232a479ca894
Vnoxygen Spike <vnoxygen@gmail.com>
2014-10-26 04:01:25 +0800

    Change from czech to english #2

--- hangupsbot/commands.py
+++ hangupsbot/commands.py
@@ -52,14 +52,14 @@ command = CommandDispatcher()
 def unknown_command(bot, event, *args):
     """Unknown command handler"""
     bot.send_message(event.conv,
-                     '{}: Ja ne znaju, ne ponimaju!'.format(event.user.full_name))
+                     '{}: Un-recognized command!'.format(event.user.full_name))
 
 
 @command.register
 def help(bot, event, cmd=None, *args):
     """Help me, Obi-Wan Kenobi. You're my only hope."""
     if not cmd:
-        segments = [hangups.ChatMessageSegment('Podporované příkazy:', is_bold=True),
+        segments = [hangups.ChatMessageSegment('Supported commands:', is_bold=True),
                     hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK),
                     hangups.ChatMessageSegment(', '.join(sorted(command.commands.keys())))]
     else:
