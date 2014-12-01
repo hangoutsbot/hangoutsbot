8914283ea0e5aaaf93cdf90f8b85a1c923313c79
null pointer <eol+temporary-2@invalidfile.name>
2014-12-01 05:36:25 -0500

    [WIP] non-functional threading, note that json "testsend" does not actually send messages

--- hangupsbot/sink2.py
+++ hangupsbot/sink2.py
@@ -1,12 +1,19 @@
+import asyncio
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+
+from hangups import event
 
 def hello(str):
     print('hello', str)
 
 def start_rpc_listener(bot):
-    bot.debug_do_something()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
     server = SimpleJSONRPCServer(('localhost', 4000))
     server.register_function(bot.debug_do_something, 'hello')
+    server.register_function(lambda: bot.rpc_list_conversations(), 'list')
+    server.register_function(lambda: bot.rpc_send_message(), 'testsend')
     server.serve_forever()
 
 if __name__ == '__main__':
