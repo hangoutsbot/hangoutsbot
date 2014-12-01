df2c876e922c836a341204a2a1bcffea4398d8aa
null pointer <eol+temporary-2@invalidfile.name>
2014-12-01 07:47:47 -0500

    interthread communication (apparently) working after sharing the BaseEventLoop

--- hangupsbot/sink2.py
+++ hangupsbot/sink2.py
@@ -1,17 +1,12 @@
 import asyncio
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 
-from hangups import event
+def start_rpc_listener(bot, loop):
+    print("started listening")
 
-def hello(str):
-    print('hello', str)
-
-def start_rpc_listener(bot):
-    loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     server = SimpleJSONRPCServer(('localhost', 4000))
-    server.register_function(bot.debug_do_something, 'hello')
     server.register_function(lambda: bot.rpc_list_conversations(), 'list')
     server.register_function(lambda: bot.rpc_send_message(), 'testsend')
     server.serve_forever()
