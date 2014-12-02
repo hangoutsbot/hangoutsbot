a4c1990d15e85261a0cb60f73c8e2f757d28df8a
null pointer <eol+temporary-2@invalidfile.name>
2014-12-02 10:49:11 -0500

    closes #1, updated rpcsend.py and removed cheap parser

--- hangupsbot/utils.py
+++ hangupsbot/utils.py
@@ -23,62 +23,6 @@ def text_to_segments(text):
 
     return segments
 
-def cheap_parse_blocks_to_segments(blocks):
-    # receives a string array containing markdown-like syntax
-    # e.g. ["'''''bold and italic ",
-    #       "'''bold only ",
-    #       "[hello world](http://google.com)"]
-    # note that the trailing space is required to avoid mashing "blocks" together
-    # XXX: needs more work for natural text input
-    segments = list()
-    for b in blocks:
-
-         """basic formatting"""
-         is_bold = False
-         is_italic = False
-         if b.startswith("'''''"):
-             is_bold = True
-             is_italic = True
-             b = b[5:]
-         elif b.startswith("'''"):
-             is_bold = True
-             b = b[3:]
-         elif b.startswith("''"):
-             is_italic = True
-             b = b[2:]
-
-         """detect line break"""
-         break_after = False
-         if b.endswith("\n"):
-            break_after = True
-
-         b = b.strip("\n")
-
-         """link"""
-         link_target = None
-         markdown_match = re.search("^\[(.*?)\]\((.*?)\)", b)
-         if markdown_match:
-             link_target = markdown_match.group(2)
-             b = markdown_match.group(1)
-         elif b.startswith(("http://", "https://", "//")):
-             link_target = b
-
-         segments.append(
-           hangups.ChatMessageSegment(
-             b, 
-             is_bold=is_bold, 
-             is_italic=is_italic, 
-             link_target=link_target))
-
-         """line break"""
-         if break_after:
-             segments.append(
-               hangups.ChatMessageSegment(
-                 "\n", 
-                 hangups.SegmentType.LINE_BREAK))
-
-    return segments
-
 class simpleHTMLParser(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -93,8 +37,6 @@ class simpleHTMLParser(HTMLParser):
         return self._segments
 
     def handle_starttag(self, tag, attrs):	
-        print("Start tag:", tag)
-
         if tag == 'b':
             self._flags["bold"] = True
         elif tag == 'i':
@@ -108,8 +50,6 @@ class simpleHTMLParser(HTMLParser):
                     break
 
     def handle_endtag(self, tag):
-        print("End tag  :", tag)
-
         if tag == 'b':
             self._flags["bold"] = False
         elif tag == 'i':
@@ -125,12 +65,6 @@ class simpleHTMLParser(HTMLParser):
                 hangups.SegmentType.LINE_BREAK))
 
     def handle_data(self, data):
-        print("Data     :", data)
-        print(" flags   : {} {} {} {}".format(
-                                         self._flags["bold"], 
-                                         self._flags["italic"],
-                                         self._flags["underline"],
-                                         self._flags["link_target"]))
         self._segments.append(
           hangups.ChatMessageSegment(
             data, 
