450b444d5aaa494b2e861ad4db6803496a995d80
null pointer <eol+temporary-2@invalidfile.name>
2014-12-28 22:06:07 +0800

    bugfix for missing segmenttype (which still fails! - see tests/)

--- hangupsbot/tests/send.py
+++ hangupsbot/tests/send.py
@@ -2,7 +2,7 @@
 import json
 import requests
 url = 'https://127.0.0.1:9001/UgwuaaLQf2IPoqZDmFZ4AaABAQ/'
-payload = {"echo" : "hello world 123"}
+payload = {"echo" : '1: <a href="https://www.google.com/">embedded</a> | 2: <a href="https://www.google.com/">www.google.com</a> | 3: https://www.google.com | 4: <a href="https://www.google.com">https://www.google.com</a>'}
 headers = {'content-type': 'application/json'}
 r = requests.post(url, data = json.dumps(payload), headers = headers, verify=False)
 print(r)
