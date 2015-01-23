34c9fc9b30cc9295922a3ea1d7774a7ca2019323
null pointer <eol+temporary-2@invalidfile.name>
2015-01-23 11:23:47 +0800

    bugfix: "&" in links would cause parser to burp and split the link feature: attempt to linkify bare urls
    (cherry picked from commit cf1eae2c978d758088878e4b6c95b0677a5e953f)

--- hangupsbot/utils.py
+++ hangupsbot/utils.py
@@ -32,12 +32,14 @@ class simpleHTMLParser(HTMLParser):
                        "underline" : False, 
                        "link_target" : None}
 
+        self._link_text = None
+
     def feed(self, html):
         self._segments = list()
         super().feed(html)
         return self._segments
 
-    def handle_starttag(self, tag, attrs):	
+    def handle_starttag(self, tag, attrs):
         if tag == 'b':
             self._flags["bold"] = True
         elif tag == 'i':
@@ -45,6 +47,7 @@ class simpleHTMLParser(HTMLParser):
         elif tag == 'u':
             self._flags["underline"] = True
         elif tag == 'a':
+            self._link_text = ""
             for attr in attrs:
                 if attr[0] == "href":
                     self._flags["link_target"] = attr[1]
@@ -58,6 +61,14 @@ class simpleHTMLParser(HTMLParser):
         elif tag == 'u':
             self._flags["underline"] = False
         elif tag == 'a':
+            self._segments.append(
+              hangups.ChatMessageSegment(
+                self._link_text,
+                hangups.SegmentType.LINK,
+                link_target=self._flags["link_target"],
+                is_bold=self._flags["bold"], 
+                is_italic=self._flags["italic"], 
+                is_underline=self._flags["underline"]))
             self._flags["link_target"] = None
         elif tag == 'br':
             self._segments.append(
@@ -67,15 +78,7 @@ class simpleHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if self._flags["link_target"] is not None:
-            print("link in message: {}".format(self._flags["link_target"]));
-            self._segments.append(
-              hangups.ChatMessageSegment(
-                data,
-                hangups.SegmentType.LINK,
-                link_target=self._flags["link_target"],
-                is_bold=self._flags["bold"], 
-                is_italic=self._flags["italic"], 
-                is_underline=self._flags["underline"]))
+            self._link_text += data 
         else:
             self._segments.append(
               hangups.ChatMessageSegment(
@@ -86,6 +89,7 @@ class simpleHTMLParser(HTMLParser):
                 link_target=self._flags["link_target"]))
 
 def simple_parse_to_segments(html):
+    html = fix_urls(html)
     parser = simpleHTMLParser()
     return parser.feed(html)
 
@@ -96,3 +100,38 @@ def class_from_name(module_name, class_name):
     # get the class, will raise AttributeError if class cannot be found
     c = getattr(m, class_name)
     return c
+
+def fix_urls(text):
+    """adapted from http://stackoverflow.com/a/1071240"""
+    pat_url = re.compile(  r'''
+                     (?x)( # verbose identify URLs within text
+         (http|ftp|gopher) # make sure we find a resource type
+                       :// # ...needs to be followed by colon-slash-slash
+            (\w+[:.]?){2,} # at least two domain groups, e.g. (gnosis.)(cx)
+                      (/?| # could be just the domain name (maybe w/ slash)
+                [^ \n\r"]+ # or stuff then space, newline, tab, quote
+                    [\w/]) # resource name ends in alphanumeric or slash
+       $|(?=[\s\.,>)'"\]]) # EOL or assert: followed by white or clause ending
+                         ) # end of match group
+                           ''')
+    pat_email = re.compile(r'''
+                    (?xm)  # verbose identify URLs in text (and multiline)
+                 (?=^.{11} # Mail header matcher
+         (?<!Message-ID:|  # rule out Message-ID's as best possible
+             In-Reply-To)) # ...and also In-Reply-To
+                    (.*?)( # must grab to email to allow prior lookbehind
+        ([A-Za-z0-9-]+\.)? # maybe an initial part: DAVID.mertz@gnosis.cx
+             [A-Za-z0-9-]+ # definitely some local user: MERTZ@gnosis.cx
+                         @ # ...needs an at sign in the middle
+              (\w+\.?){2,} # at least two domain groups, e.g. (gnosis.)(cx)
+         (?=[\s\.,>)'"\]]) # assert: followed by white or clause ending
+                         ) # end of match group
+                           ''')
+
+    for url in re.findall(pat_url, text):
+       text = text.replace(url[0], '<a href="%(url)s">%(url)s</a>' % {"url" : url[0]})
+
+    for email in re.findall(pat_email, text):
+       text = your_string.replace(email[1], '<a href="mailto:%(email)s">%(email)s</a>' % {"email" : email[1]})
+
+    return text
