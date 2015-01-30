9b1db9ba3f691bc18167cbb837f42a02c4e67faa
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-01-30 15:00:07 +0800

    Added temporary code until plugins can support renames

--- hangupsbot/plugins/namelock.py
+++ hangupsbot/plugins/namelock.py
@@ -1,6 +1,21 @@
 """Allows the user to configure the bot to watch for hangout renames
 and change the name back to a default name accordingly"""
 
+import asyncio
+
+import hangups
+
+def _initialise(command):
+    command.register_handler(_handle_rename)
+    return ["topic"]
+
+@asyncio.coroutine
+def _handle_rename(bot, event, command):
+    """handle renames"""
+    print("Handling rename!")
+    if isinstance(event, hangups.RenameEvent):
+        yield from bot._client.setchatname(event.conv_id, bot.memory.get_by_path(["conv_data", event.conv_id, "topic"]))
+
 def topic(bot, event, *args):
     """Set a chat topic. If no parameters given, remove the topic"""
 
@@ -20,4 +35,4 @@ def topic(bot, event, *args):
             "Setting topic to '{}'".format(topic))
 
     """Rename Hangout"""
-    yield from bot._client.setchatname(event.conv_id, ' '.join(args))
+    yield from bot._client.setchatname(event.conv_id, topic)
