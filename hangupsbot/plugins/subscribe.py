77969c6752d86ffe0da32f8358712f7f87db5b75
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-02-02 10:20:15 +0800

    Allowed multiple keywords to be read

--- hangupsbot/plugins/subscribe.py
+++ hangupsbot/plugins/subscribe.py
@@ -8,13 +8,15 @@ def _initialise(command):
     return ["subscribe", "unsubscribe"]
 
 def _populate_keywords(bot, event):
-    # Pull the keywords from file
+    # Pull the keywords from file if not already
     if not keywords:
         bot.initialise_memory(event.user.id_.chat_id, "user_data")
         for userchatid in bot.memory.get_option("user_data"):
             userkeywords = bot.memory.get_suboption("user_data", userchatid, "keywords")
             if userkeywords:
-                keywords.append(userkeywords)
+                keywords[userchatid] = userkeywords
+            else:
+                keywords[userchatid] = []
 
 @asyncio.coroutine
 def _handle_keyword(bot, event, command):
@@ -31,16 +33,16 @@ def subscribe(bot, event, *args):
             event.conv,"Usage: /bot subscribe <keyword>")
         return
 
-    # Check for duplicates
+    # Check for duplicates, if none, append the keyword
     if keywords:
         if keyword in keywords[event.user.id_.chat_id]:
             bot.send_message_parsed(
                 event.conv,"Already subscribed to '{}'!".format(keyword))
             return
-
-
-    # Add to cache
-    keywords[event.user.id_.chat_id].append(keyword)
+        elif not keywords[event.user.id_.chat_id]:
+            keywords[event.user.id_.chat_id] = [keyword]
+        else:
+            keywords[event.user.id_.chat_id].append(keyword)
 
     # Save to file
     bot.memory.set_by_path(["user_data", event.user.id_.chat_id, "keywords"], keywords[event.user.id_.chat_id])
