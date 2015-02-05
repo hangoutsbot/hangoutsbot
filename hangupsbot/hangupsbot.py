74d6db0e323c98a813bfc3a6bbb42882a5444810
null pointer <eol+temporary-2@invalidfile.name>
2015-02-05 19:57:41 +0800

    bumped version number

--- hangupsbot/hangupsbot.py
+++ hangupsbot/hangupsbot.py
@@ -17,7 +17,7 @@ from sinks.listener import start_listening
 from inspect import getmembers, isfunction
 
 
-__version__ = '1.1'
+__version__ = '2.1'
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 class FakeConversation(object):
