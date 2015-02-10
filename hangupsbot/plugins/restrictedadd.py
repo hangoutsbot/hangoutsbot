0deb4188c77f83eb2bf50b68c8b55bec5733cd17
null pointer <eol+temporary-2@invalidfile.name>
2015-02-10 12:36:27 +0800

    workaround for #71 - bot now verifies for presence of at least 1 admin or a designated "botkeeper" (/bot allowbotadd <id>) on message received after at least 60 seconds have elapsed since last message/check

--- hangupsbot/plugins/restrictedadd.py
+++ hangupsbot/plugins/restrictedadd.py
@@ -1,11 +1,22 @@
 import asyncio
 import hangups
+import time
 
 from hangups.ui.utils import get_conv_name
 
+class __internal_vars():
+    def __init__(self):
+        self.last_verified = {}
+
+
+_internal = __internal_vars()
+
+
 def _initialise(Handlers, bot=None):
     Handlers.register_handler(_check_if_admin_added_me, type="membership")
+    Handlers.register_handler(_verify_botkeeper_presence, type="message")
     return ["allowbotadd", "removebotadd"]
+
 
 @asyncio.coroutine
 def _check_if_admin_added_me(bot, event, command):
@@ -25,7 +36,7 @@ def _check_if_admin_added_me(bot, event, command):
                 print("RESTRICTEDADD: admin added me to {}".format(
                     event.conv_id))
             elif initiator_user_id in allowbotadd:
-                # bot added by an admin
+                # bot added by an authorised user: /bot allowbotadd <id>
                 print("RESTRICTEDADD: authorised user added me to {}".format(
                     event.conv_id))
             else:
@@ -37,8 +48,58 @@ def _check_if_admin_added_me(bot, event, command):
                     event.conv, 
                     "<i>{}, you need to be authorised to add me to another conversation. I'm leaving now...</i>".format(event.user.full_name))
 
-                yield from asyncio.sleep(1.0)
-                yield from command.run(bot, event, *["leave", "quietly"])
+                yield from _leave_the_chat_quietly(bot, event, command)
+
+
+@asyncio.coroutine
+def _verify_botkeeper_presence(bot, event, command):
+    if not bot.get_config_option('strict_botkeeper_check'):
+        return
+
+    try:
+        if time.time() - _internal.last_verified[event.conv_id] < 60:
+            # don't check on every event
+            return
+    except KeyError:
+        # not set - first time, so do a check
+        pass
+
+    if len(event.conv.users) < 3:
+        # groups only!
+        return
+
+    admins_list = bot.get_config_suboption(event.conv_id, 'admins')
+    if bot.memory.exists(["allowbotadd"]):
+        allowbotadd = bot.memory.get("allowbotadd")
+    else:
+        allowbotadd = []
+
+    botkeeper = False
+    for user in event.conv.users:
+        if user.id_.chat_id in admins_list or user.id_.chat_id in allowbotadd:
+            # at least one user is a botkeeper
+            print("RESTRICTEDADD: found botkeeper {}".format(user.id_.chat_id))
+            botkeeper = True
+            break
+
+    _internal.last_verified[event.conv_id] = time.time()
+
+    if not botkeeper:
+        print("RESTRICTEDADD: no botkeeper in {}".format(
+            event.conv_id))
+
+        bot.send_message_parsed(
+            event.conv, 
+            "<i>There is no botkeeper in here. I have to go...</i>")
+
+        yield from _leave_the_chat_quietly(bot, event, command)
+
+
+@asyncio.coroutine
+def _leave_the_chat_quietly(bot, event, command):
+    yield from asyncio.sleep(1.0)
+    yield from command.run(bot, event, *["leave", "quietly"])
+
 
 def allowbotadd(bot, event, user_id, *args):
     if not bot.memory.exists(["allowbotadd"]):
@@ -48,10 +109,12 @@ def allowbotadd(bot, event, user_id, *args):
     allowbotadd.append(user_id)
     bot.send_message_parsed(
         event.conv,
-        "user id {} authorised to add bot in any conversations".format(user_id))
-
+        "user id {} added as botkeeper".format(user_id))
     bot.memory["allowbotadd"] = allowbotadd
     bot.memory.save()
+
+    _internal.last_verified = {} # force checks everywhere
+
 
 def removebotadd(bot, event, user_id, *args):
     if not bot.memory.exists(["allowbotadd"]):
@@ -62,10 +125,12 @@ def removebotadd(bot, event, user_id, *args):
         allowbotadd.remove(user_id)
         bot.send_message_parsed(
             event.conv,
-            "user id {} authorisation to add bot in any conversations removed".format(user_id))
+            "user id {} removed as botkeeper".format(user_id))
 
         bot.memory["allowbotadd"] = allowbotadd
         bot.memory.save()
+
+        _internal.last_verified = {} # force checks everywhere
     else:
         bot.send_message_parsed(
             event.conv,
