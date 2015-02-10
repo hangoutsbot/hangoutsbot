e6d27df7b905a7237f63c390db7e5dba86d14f5f
null pointer <eol+temporary-2@invalidfile.name>
2015-02-10 17:50:06 +0800

    /bot synclanguage changed to /bot roomlanguage use either the 2-letter ISO code ALL-CAPS, or any-cased language full name

--- hangupsbot/plugins/syncrooms_autotranslate.py
+++ hangupsbot/plugins/syncrooms_autotranslate.py
@@ -8,16 +8,18 @@ gs = goslate.Goslate()
 
 def _initialise(Handlers, bot=None):
     Handlers.register_handler(_translate_message, type="sending")
+    return ['roomlanguage']
 
 
 def _translate_message(bot, broadcast_list, context):
     if context and "autotranslate" in context:
+        print(len(broadcast_list))
         _autotranslate = context["autotranslate"]
-        origin_language = _get_syncroom_language(bot, _autotranslate["conv_id"])
+        origin_language = _get_room_language(bot, _autotranslate["conv_id"])
         for send in broadcast_list:
             target_conversation_id = send[0]
             response = send[1]
-            target_language = _get_syncroom_language(bot, target_conversation_id)
+            target_language = _get_room_language(bot, target_conversation_id)
             if origin_language != target_language:
                 print("AUTOTRANSLATE(): translating {} to {}".format(origin_language, target_language))
                 translated = gs.translate(_autotranslate["event_text"], target_language)
@@ -28,7 +30,7 @@ def _translate_message(bot, broadcast_list, context):
                         hangups.ChatMessageSegment('(' + translated + ')')])
 
 
-def _get_syncroom_language(bot, conversation_id, default="en"):
+def _get_room_language(bot, conversation_id, default="en"):
     syncroom_language = bot.conversation_memory_get(conversation_id, 'syncroom_language')
     if syncroom_language is None:
         return default
@@ -36,23 +38,29 @@ def _get_syncroom_language(bot, conversation_id, default="en"):
         return syncroom_language
 
 
-def synclanguage(bot, event, iso_language=None, *args):
+def roomlanguage(bot, event, *args):
     language_map = gs.get_languages()
 
-    if iso_language is None:
-        bot.send_message_parsed(
-            event.conv,
-            '<i>syncroom "{}" language is {}</i>'.format(
-                get_conv_name(event.conv),
-                language_map[_get_syncroom_language(bot, event.conv_id)]))
+    language = " ".join(args)
 
-    if iso_language in language_map:
+    if not language:
+        try:
+            bot.send_message_parsed(
+                event.conv,
+                '<i>syncroom "{}" language is {}</i>'.format(
+                    get_conv_name(event.conv),
+                    language_map[_get_room_language(bot, event.conv_id)]))
+        except KeyError:
+            pass
+        return
+
+    for iso_language in language_map:
         text_language = language_map[iso_language]
-
-        bot.conversation_memory_set(event.conv_id, 'syncroom_language', iso_language)
-
-        bot.send_message_parsed(
-            event.conv,
-            '<i>syncroom "{}" language set to {}</i>'.format(
-                get_conv_name(event.conv),
-                text_language))
+        if language.lower() in text_language.lower() or language == iso_language.upper():
+            bot.conversation_memory_set(event.conv_id, 'syncroom_language', iso_language)
+            bot.send_message_parsed(
+                event.conv,
+                '<i>syncroom "{}" language set to {}</i>'.format(
+                    get_conv_name(event.conv),
+                    text_language))
+            break
