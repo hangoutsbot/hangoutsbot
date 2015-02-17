ed319f2efe76adc48a0e8e457916a082e44f8132
null pointer <eol+temporary-2@invalidfile.name>
2015-02-17 11:25:31 +0800

    upgraded get_users_in_conversation(), accepts list of conversation ids; added syncroom-aware users command /bot syncusers to syncroom plugin

--- hangupsbot/plugins/syncrooms.py
+++ hangupsbot/plugins/syncrooms.py
@@ -13,12 +13,15 @@ class __registers(object):
 
 _registers=__registers()
 
+
 def _initialise(Handlers, bot=None):
     _migrate_syncroom_v1(bot)
+    Handlers.register_admin_command(["syncusers"])
     Handlers.register_handler(_handle_syncrooms_broadcast, type="sending")
     Handlers.register_handler(_handle_incoming_message, type="message")
     Handlers.register_handler(_handle_syncrooms_membership_change, type="membership")
     return [] # implements no commands
+
 
 def _migrate_syncroom_v1(bot):
     if bot.config.exists(["conversations"]):
@@ -45,6 +48,7 @@ def _migrate_syncroom_v1(bot):
             bot.config.set_by_path(["sync_rooms"], _config2) # write new config
             bot.config.save()
             print("_migrate_syncroom_v1(): config-v2 = {}".format(_config2))
+
 
 def _handle_syncrooms_broadcast(bot, broadcast_list, context):
     """
@@ -222,3 +226,70 @@ def _handle_syncrooms_membership_change(bot, event, command):
             bot.send_message_parsed(event.conv, '<i>{} has left {}</i>'.format(
                 names,
                 syncroom_name))
+
+
+def syncusers(bot, event, conversation_id=None, *args):
+    """syncroom-aware users list.
+    optional parameter conversation_id to get a list of users in other rooms. will include users
+    in linked syncrooms. append "rooms" to segment user list by individual rooms.
+    """
+    combined = True
+
+    if not conversation_id:
+        conversation_id = event.conv_id
+    elif conversation_id == "rooms":
+        # user specified /bot syncusers rooms
+        conversation_id = event.conv_id
+        combined = False
+
+    if "rooms" in args:
+        # user specified /bot syncusers [roomid] rooms
+        combined = False
+
+    syncouts = bot.get_config_option('sync_rooms')
+
+    if not syncouts:
+        return # Sync rooms not configured, returning
+
+    _lines = []
+
+    # are we in a sync room?
+    sync_room_list = None
+    for _rooms in syncouts:
+        if conversation_id in _rooms:
+            sync_room_list = _rooms
+            _lines.append("<b>Sync Rooms: {}</b>".format(len(sync_room_list)))
+            break
+    if sync_room_list is None:
+        sync_room_list = [conversation_id]
+        _lines.append("<b>Standard Room</b>")
+
+    all_users = {}
+    if combined or len(sync_room_list) == 1:
+        all_users["_ALL_"] = bot.get_users_in_conversation(sync_room_list)
+    else:
+        for room_id in sync_room_list:
+            all_users[room_id] = bot.get_users_in_conversation(room_id)
+
+    unique_users = []
+
+    for room_id in all_users:
+        if room_id is not "_ALL_":
+            _line_room = '<i>{}</i>'.format(room_id)
+            """XXX: written in this way in case we need to try: get_conv_name in future"""
+            _line_room = '<b>{}</b> {}'.format(
+                get_conv_name(bot._conv_list.get(room_id)), 
+                _line_room)
+            _lines.append(_line_room)
+        list_users = all_users[room_id]
+        for User in list_users:
+            _line_user = '{}'.format(User.full_name);
+            if User.emails:
+                _line_user = _line_user + ' ({})'.format(User.emails[0])
+            _lines.append(_line_user)
+            unique_users.append(User)
+
+    unique_users = list(set(unique_users))
+    _lines.append("<b>Total Unique: {}</b>".format(len(unique_users)))
+
+    bot.send_message_parsed(event.conv, '<br />'.join(_lines))
