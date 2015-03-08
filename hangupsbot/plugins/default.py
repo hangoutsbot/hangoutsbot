ac5ed53096a7dfa2e0662a0ebbc9ef07442f5e72
jfx41 <jfa@packetdamage.com>
2015-03-07 19:21:48 -0500

    Remove memory reload. Pushed to master instead of staging.

--- hangupsbot/plugins/default.py
+++ hangupsbot/plugins/default.py
@@ -133,7 +133,6 @@ def leave(bot, event, conversation_id=None, *args):
 def reload(bot, event, *args):
     """reload config"""
     bot.config.load()
-    bot.memory.load()
 
 
 def quit(bot, event, *args):
