758a581921df9447e8773ceac5bd717b8e47d3d5
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-03-20 18:15:15 +1100

    Updated legacy code (from segment sending to html sending)

--- hangupsbot/plugins/lookup.py
+++ hangupsbot/plugins/lookup.py
@@ -20,9 +20,8 @@ def lookup(bot, event, *args):
 
     keyword = ' '.join(args)
 
-    segments = [hangups.ChatMessageSegment('Results for keyword "{}":'.format(keyword),
-                                           is_bold=True),
-                hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK)]
+    htmlmessage = 'Results for keyword <b>{}</b>:<br />'.format(keyword)
+
     print("{0} ({1}) has requested to lookup '{2}'".format(event.user.full_name, event.user.id_.chat_id, keyword))
     import urllib.request
     html = urllib.request.urlopen(spreadsheet_url).read()
@@ -53,23 +52,18 @@ def lookup(bot, event, *args):
         for cell in row:
             testcell = str(cell).lower().strip()
             if (keyword_lower in testcell) and counter < counter_max and matchfound == 0:
-                segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-                segments.append(hangups.ChatMessageSegment('Row {}: '.format(counter+1),
-                                                       is_bold=True))
+                htmlmessage += '<br />Row {}: '.format(counter+1)
                 for datapoint in row:
-                    segments.append(hangups.ChatMessageSegment(datapoint))
-                    segments.append(hangups.ChatMessageSegment(' | ', is_bold=True))
-                segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
+                    htmlmessage += '{} | '.format(datapoint)
+                htmlmessage += '<br />'
                 counter += 1
                 matchfound = 1
             elif (keyword_lower in testcell) and counter >= counter_max:
                 counter += 1
 
     if counter > counter_max:
-        segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-        segments.append(hangups.ChatMessageSegment('{0} rows found. Only returning first {1}.'.format(counter, counter_max), is_bold=True))
+        htmlmessage += '<br />{0} rows found. Only returning first {1}.'.format(counter, counter_max)
     if counter == 0:
-        segments.append(hangups.ChatMessageSegment('No match found', is_bold=True))
-
-    segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-    bot.send_message_segments(event.conv, segments)
+        htmlmessage += 'No match found'
+        
+    bot.send_html_to_conversation(event.conv, htmlmessage)
