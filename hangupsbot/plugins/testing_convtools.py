d67578d1cdf8aefd2ff3717422f497e57a12237e
null pointer <eol+temporary-2@invalidfile.name>
2015-03-30 20:19:11 +0800

    apparently the initiator id is not required

--- hangupsbot/plugins/testing_convtools.py
+++ hangupsbot/plugins/testing_convtools.py
@@ -18,11 +18,8 @@ def addusers(bot, event, *args):
     yield from bot._client.adduser(current_conv_id, user_ids)
 
 def createconversation(bot, event, *args):
-    user_ids = list(args)
-    user_ids.insert(0, bot.user_self()["chat_id"])
-    user_ids = list(set(user_ids))
+    user_ids = list(set(args))
     print("createconversation: {}".format(user_ids))
     response = yield from bot._client.createconversation(user_ids)
-    print("createconversation: {}".format(response))
     new_conversation_id = response['conversation']['id']['id']
     bot.send_html_to_conversation(new_conversation_id, "<i>conversation created</i>")
