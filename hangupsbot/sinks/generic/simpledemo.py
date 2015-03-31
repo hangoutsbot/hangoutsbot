6dde6e558d1dc39bfe2ee11c2f9d83934330932b
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-03-31 13:52:26 +1100

    Added gettext for base .py modules and sinks

--- hangupsbot/sinks/generic/simpledemo.py
+++ hangupsbot/sinks/generic/simpledemo.py
@@ -10,7 +10,7 @@ class webhookReceiver(BaseHTTPRequestHandler):
         path = path.split("/")
         conversation_id = path[1]
         if conversation_id is None:
-            print("conversation id must be provided as part of path")
+            print(_("conversation id must be provided as part of path"))
             return
 
         if "echo" in payload:
@@ -24,7 +24,7 @@ class webhookReceiver(BaseHTTPRequestHandler):
         """
             receives post, handles it
         """
-        print('receiving POST...')
+        print(_('receiving POST...'))
         data_string = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
         self.send_response(200)
         message = bytes('OK', 'UTF-8')
@@ -32,14 +32,14 @@ class webhookReceiver(BaseHTTPRequestHandler):
         self.send_header("Content-length", str(len(message)))
         self.end_headers()
         self.wfile.write(message)
-        print('connection closed')
+        print(_('connection closed'))
 
         # parse requested path + query string
         _parsed = urlparse(self.path)
         path = _parsed.path
         query_string = parse_qs(_parsed.query)
 
-        print("incoming path: {}".format(path))
+        print(_("incoming path: {}").format(path))
 
         print(data_string)
 
--- hangupsbot/sinks/google/scripts.py
+++ hangupsbot/sinks/google/scripts.py
@@ -10,7 +10,7 @@ class webhookReceiver(BaseHTTPRequestHandler):
         path = path.split("/")
         conversation_id = path[1]
         if conversation_id is None:
-            print("conversation id must be provided as part of path")
+            print(_("conversation id must be provided as part of path"))
             return
 
         if "message" in payload:
@@ -18,7 +18,7 @@ class webhookReceiver(BaseHTTPRequestHandler):
         else:
             print(payload)
 
-        print("handler finished")
+        print(_("handler finished"))
 
     def _scripts_push(self, conversation_id, payload):
         try:
@@ -30,7 +30,7 @@ class webhookReceiver(BaseHTTPRequestHandler):
         """
             receives post, handles it
         """
-        print('receiving POST...')
+        print(_('receiving POST...'))
         data_string = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
         self.send_response(200)
         message = bytes('OK', 'UTF-8')
@@ -38,18 +38,18 @@ class webhookReceiver(BaseHTTPRequestHandler):
         self.send_header("Content-length", str(len(message)))
         self.end_headers()
         self.wfile.write(message)
-        print('connection closed')
+        print(_('connection closed'))
 
         # parse requested path + query string
         _parsed = urlparse(self.path)
         path = _parsed.path
         query_string = parse_qs(_parsed.query)
 
-        print("incoming path: {}".format(path))
+        print(_("incoming path: {}").format(path))
 
         # parse incoming data
         payload = json.loads(data_string)
 
-        print("payload {}".format(payload))
+        print(_("payload {}").format(payload))
 
         self._handle_incoming(path, query_string, payload)
