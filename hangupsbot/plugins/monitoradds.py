d7755875173e49585fd5f0b98c70e5e22808ab86
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-04-04 15:50:57 +1100

    Added error handling for when mods is not defined

--- hangupsbot/plugins/monitoradds.py
+++ hangupsbot/plugins/monitoradds.py
@@ -3,7 +3,7 @@ Plugin for monitoring new adds to HOs and alerting if users were not added by an
 Add mods to the config.json file either globally or on an individual HO basis.
 Add a "watch_new_adds": true  parameter to individual HOs in the config.json file.
 
-Author: @Riptides 
+Author: @Riptides
 """
 
 import asyncio
@@ -28,11 +28,18 @@ def _watch_new_adds(bot, event, command):
     # JOIN
     if event.conv_event.type_ == hangups.MembershipChangeType.JOIN:
         # Check if the user who added people is a mod or admin
-        mods_list = bot.get_config_suboption(event.conv_id, 'mods')
         admins_list = bot.get_config_suboption(event.conv_id, 'admins')
-        if event.user_id.chat_id in admins_list or event.user_id.chat_id in mods_list:
+        if event.user_id.chat_id in admins_list:
             return
-        else:
-            bot.send_message_parsed(event.conv, '<b>!!! Warning !!!</b>')
-            bot.send_message_parsed(event.conv, '<i>{}, invited user {} without authorization!'.format(event.user.full_name, names))
-            bot.send_message_parsed(event.conv, '<i>{}: Please leave this hangout and ask a moderator to add you. Thank you for your understanding.'.format(names))
+
+        mods_list = bot.get_config_suboption(event.conv_id, 'mods')
+        try:
+            if event.user_id.chat_id in mods_list:
+                return
+        except TypeError:
+            # The mods are likely not configured. Continuing...
+            pass
+
+        bot.send_message_parsed(event.conv, '<b>!!! Warning !!!</b>')
+        bot.send_message_parsed(event.conv, '<i>{}, invited user {} without authorization!'.format(event.user.full_name, names))
+        bot.send_message_parsed(event.conv, '<i>{}: Please leave this hangout and ask a moderator to add you. Thank you for your understanding.'.format(names))
