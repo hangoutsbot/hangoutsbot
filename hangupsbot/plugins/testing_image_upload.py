a09fe71f062a014fcd531c0a2d9011cc8e729957
null pointer <eol+temporary-2@invalidfile.name>
2015-04-06 11:25:29 +0800

    downloading image no longer required, properly parse imgur links

--- hangupsbot/plugins/testing_image_upload.py
+++ hangupsbot/plugins/testing_image_upload.py
@@ -2,11 +2,7 @@
 Identify images, upload them to google plus, post in hangouts
 """
 
-import asyncio, aiohttp, asyncio, os
-
-import hangups, json, random
-
-from urllib.parse import urlparse
+import asyncio, aiohttp, asyncio, os, io
 
 def _initialise(Handlers, bot=None):
     Handlers.register_handler(_watch_image_link, type="message")
@@ -21,20 +17,21 @@ def _watch_image_link(bot, event, command):
 
     # Detecting a photo
     if (".jpg" in event.text or "imgur.com" in event.text or ".png" in event.text or ".gif" in event.text or ".gifv" in event.text) and "googleusercontent" not in event.text:
+
         if("imgur.com" in event.text and ".jpg" not in event.text and ".gif" not in event.text and ".gifv" not in event.text and ".png" not in event.text):
             event.text = event.text + ".gif"
+            if "/gallery/" in event.text:
+                event.text = event.text.replace("/gallery/", "/")
 
-        # We need to download the photo first before we can upload it
         downloadURL = event.text.replace(".gifv",".gif")
-        fileName = os.path.basename(urlparse(downloadURL).path)
-        r = yield from aiohttp.request('get',downloadURL)
+
+        print("image(): getting {}".format(downloadURL))
+
+        filename = os.path.basename(downloadURL)
+        r = yield from aiohttp.request('get', downloadURL)
         raw = yield from r.read()
-        newFile = open(fileName,'wb')
-        newFile.write(raw)
+        image_data = io.BytesIO(raw)
 
-        photoID = yield from bot._client.upload_image(fileName)
+        image_id = yield from bot._client.upload_image(image_data, filename=filename)
 
-        yield from bot._client.sendchatmessage(event.conv.id_, None, imageID=photoID)
-
-        # Remove the image after use
-        os.remove(fileName)
+        yield from bot._client.sendchatmessage(event.conv.id_, None, image_id=image_id)
