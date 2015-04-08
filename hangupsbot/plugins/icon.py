08cddd0b93550c5b66c53c27bce5ae2ae7b74ef4
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-04-08 15:37:20 +1000

    Added slack.py plugin to sync slack messages

---
+++ hangupsbot/plugins/icon.py
@@ -0,0 +1,10 @@
+import hangups
+
+def geticon(bot, event, *args):
+    response = yield from bot._client.getentitybyid([event.user_id.chat_id])
+    try:
+        photo_url = "http:" + response['entity'][0]['properties']['photo_url']
+    except Exception as e:
+        print("refreshusers() {} {}".format(event.user_id.chat_id, e))
+
+    bot.send_html_to_conversation(event.conv_id, photo_url)
