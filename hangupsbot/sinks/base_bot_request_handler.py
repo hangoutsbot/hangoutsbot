003b93b80e8111a0a3a7f8740836c4d898ef8ea8
null pointer <eol+temporary-2@invalidfile.name>
2015-04-20 09:16:08 +0800

    refactored request handler to be easier to override docstrings added for developer reference

--- hangupsbot/sinks/base_bot_request_handler.py
+++ hangupsbot/sinks/base_bot_request_handler.py
@@ -18,17 +18,56 @@ class BaseBotRequestHandler(BaseHTTPRequestHandler):
         self.sinkname = self.__class__.__name__
         BaseHTTPRequestHandler.__init__(self, *args)
 
+
+    def do_POST(self):
+        """handle incoming POST request
+        acquire the path, any query string (?abc=xyz), sent content
+        """
+        print('{}: receiving POST...'.format(self.sinkname))
+
+        content = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
+        self.send_response(200)
+        message = bytes('OK', 'UTF-8')
+        self.send_header("Content-type", "text")
+        self.send_header("Content-length", str(len(message)))
+        self.end_headers()
+        self.wfile.write(message)
+        print('{}: connection closed'.format(self.sinkname))
+
+        # parse requested path + query string
+        _parsed = urlparse(self.path)
+        path = _parsed.path
+        query_string = parse_qs(_parsed.query)
+
+        print("{}: incoming: {} {} {} bytes".format(self.sinkname, path, query_string, len(content)))
+
+        # process the payload
+        asyncio.async(self.process_request(path, query_string, content))
+
+
     @asyncio.coroutine
-    def process_payload(self, path, query_string, payload):
-        sinkname = self.sinkname
+    def process_request(self, path, query_string, content):
+        """default handler for incoming request
+        path should contain a conversation id e.g. http://localhost/XXXXXXXXXXX/
+        content is a valid json string with keys:
+            echo                html string
+            image 
+                base64encoded   base64-encoded image data
+                filename        optional filename
+        """
+        # parse incoming data
+        payload = json.loads(content)
 
         path = path.split("/")
         conversation_id = path[1]
         if conversation_id is None:
-            print("{}: conversation id must be provided as part of path".format(sinkname))
+            print("{}: conversation id must be provided as part of path".format(self.sinkname))
             return
 
-        image_id = None
+        html = None
+        if "echo" in payload:
+            html = payload["echo"]
+
         if "image" in payload:
             image_data = False
             image_filename = False
@@ -37,51 +76,34 @@ class BaseBotRequestHandler(BaseHTTPRequestHandler):
                 image_data = io.BytesIO(raw)
             if "filename" in payload["image"]:
                 image_filename = payload["image"]["filename"]
-            else:
+
+        if not html and not image_data:
+            print("{}: nothing to send".format(self.sinkname))
+            return
+
+        yield from self.send_data(conversation_id, html, image_data=image_data, image_filename=image_filename)
+
+
+    @asyncio.coroutine
+    def send_data(self, conversation_id, html, image_data=None, image_filename=None):
+        """sends html and/or image to a conversation
+        image_filename is optional, defaults to <timestamp>.jpg if not defined
+        """
+        image_id = None
+        if image_data:
+            if not image_filename:
                 image_filename = str(int(time.time())) + ".jpg"
-            print("{}: uploading image: {}".format(sinkname, image_filename))
             image_id = yield from self._bot._client.upload_image(image_data, filename=image_filename)
 
-        html = None
-        if "echo" in payload:
-            html = payload["echo"]
-
         if not html and not image_id:
-            print("{}: nothing to send".format(sinkname))
+            print("{}: nothing to send".format(self.sinkname))
             return
 
         segments = simple_parse_to_segments(html)
-        print("{}: sending segments: {}".format(sinkname, len(segments)))
+        print("{}: sending segments: {}".format(self.sinkname, len(segments)))
 
         self._bot.send_message_segments(conversation_id, segments, context=None, image_id=image_id)
 
-    def do_POST(self):
-        sinkname = self.sinkname
-
-        print('{}: receiving POST...'.format(sinkname))
-
-        data_string = self.rfile.read(int(self.headers['Content-Length'])).decode('UTF-8')
-        self.send_response(200)
-        message = bytes('OK', 'UTF-8')
-        self.send_header("Content-type", "text")
-        self.send_header("Content-length", str(len(message)))
-        self.end_headers()
-        self.wfile.write(message)
-        print('{}: connection closed'.format(sinkname))
-
-        # parse requested path + query string
-        _parsed = urlparse(self.path)
-        path = _parsed.path
-        query_string = parse_qs(_parsed.query)
-
-        print("{}: incoming path: {}".format(sinkname, path))
-        print("{}: incoming data: approx {} bytes".format(sinkname, len(data_string)))
-
-        # parse incoming data
-        payload = json.loads(data_string)
-
-        # process the payload
-        asyncio.async(self.process_payload(path, query_string, payload))
 
     def log_error(self, format_string, *args):
         logging.error("{} - {} {}".format(self.sinkname, self.address_string(), format_string%args))
