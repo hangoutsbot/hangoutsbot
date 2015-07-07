3057a2dcdd17d8de147ea590317be50f2b2ce162
null pointer <eol+temporary-2@invalidfile.name>
2015-07-07 09:36:01 +0800

    added convfilter, part of #222 removed backward compatibility for bot < 2.4

--- hangupsbot/plugins/default.py
+++ hangupsbot/plugins/default.py
@@ -11,21 +11,40 @@ _internal = {} # non-persistent internal state independent of config.json/memory
 
 _internal["broadcast"] = { "message": "", "conversations": [] } # /bot broadcast
 
-def _initialise(Handlers, bot=None):
-    admin_commands = ["broadcast", "users", "user", "hangouts", "rename", "leave", "reload", "quit", "config", "whereami"]
-    user_commands = ["echo", "echoparsed", "whoami"]
-    try:
-        plugins.register_admin_command(admin_commands)
-        plugins.register_user_command(user_commands)
-    except Exception as e:
-        if "register_admin_command" in dir(Handlers) and "register_user_command" in dir(Handlers):
-            print(_("DEFAULT: LEGACY FRAMEWORK MODE"))
-            Handlers.register_admin_command(admin_commands)
-            Handlers.register_user_command(user_commands)
-        else:
-            print(_("DEFAULT: OBSOLETE FRAMEWORK MODE"))
-            return admin_commands + user_commands
-    return []
+def _initialise(bot):
+    plugins.register_admin_command(["broadcast", "convfilter", "users", "user", "hangouts", "rename", "leave", "reload", "quit", "config", "whereami"])
+    plugins.register_user_command(["echo", "echoparsed", "whoami"])
+
+def _filter_convlist(bot, fragment):
+    # get definitive list of all conversations
+    convs = get_all_conversations()
+
+    filtered = [] # function always return tuple(convid, convdata)
+
+    if fragment in convs:
+        # prioritise exact convid matches
+        filtered.append((fragment, convs[fragment]))
+    else:
+        # perform case-insensitive search
+        fragment_lower = fragment.lower()
+        for convid, convdata in convs.items():
+            if fragment_lower in convdata["title"].lower():
+                filtered.append((convid, convdata))
+
+    return filtered
+
+
+def convfilter(bot, event, *args):
+    fragment = ' '.join(args)
+
+    convlist = _filter_convlist(bot, fragment)
+
+    lines = []
+    for convid, convdata in convlist:
+        lines.append("{} <b>{}</b>".format(convid, convdata["title"], len(convdata["users"])))
+    lines.append("Total: {}".format(len(lines)))
+
+    bot.send_message_parsed(event.conv_id, '<br />'.join(lines))
 
 
 def echo(bot, event, *args):
