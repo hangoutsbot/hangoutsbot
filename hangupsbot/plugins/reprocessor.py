83fa87a91c1efcaea6c9ff24f82aac56d9cae3a8
null pointer <eol+temporary-2@invalidfile.name>
2015-07-10 20:19:24 +0800

    merged reprocessor as framework feature, closes #218 reprocessor plugin reduced to test script

--- hangupsbot/plugins/reprocessor.py
+++ hangupsbot/plugins/reprocessor.py
@@ -2,37 +2,8 @@ import hangups
 
 import plugins
 
-import uuid
-
-
-_reprocessors = {}
-
-prefix = "uuid://"
-
 def _initialise(bot):
     plugins.register_admin_command(["testcontext"])
-    plugins.register_handler(_scan_for_reprocessor_context, type="allmessages")
-    plugins.register_shared('reprocessor.attach_reprocessor', _attach_reprocessor)
-
-
-def _attach_reprocessor(func):
-    _id = str(uuid.uuid4())
-    _reprocessors[_id] = func
-    context_fragment = '<a href="' + prefix + _id + '"> </a>'
-    return context_fragment
-
-
-def _scan_for_reprocessor_context(bot, event, command):
-    if len(event.conv_event.segments) > 0:
-        for segment in event.conv_event.segments:
-            if segment.link_target:
-                if segment.link_target.startswith(prefix):
-                    _id = segment.link_target[len(prefix):]
-                    if _id in _reprocessors:
-                        print("valid uuid found: {}".format(_id))
-                        _reprocessors[_id](bot, event, _id)
-                        del _reprocessors[_id]
-
 
 def testcontext(bot, event, *args):
     """test hidden context"""
