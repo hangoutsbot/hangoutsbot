902464ff859fdafb4042f5d86d534616c593e183
null pointer <eol+temporary-2@invalidfile.name>
2015-07-21 13:46:19 +0800

    permanent user memory by storing hangups User attributes in memory.user_data intelligent migration for existing memory convmem.participants is a list based on convmem.users

--- hangupsbot/utils.py
+++ hangupsbot/utils.py
@@ -68,14 +68,18 @@ class conversation_memory:
         sys.modules[__name__].bot = bot # workaround for drop-ins
 
     def load_from_hangups(self):
-        logging.info("conversation_memory(): loading from hangups")
-        for conv in self.bot._conv_list.get_all():
-            self.update(conv, source="init", automatic_save=False)
+        logging.info("convmem: loading users from hangups")
+        for User in self.bot._user_list.get_all():
+            self.store_user_memory(User, automatic_save=False, is_definitive=True)
+
+        logging.info("convmem: loading conversations from hangups")
+        for Conversation in self.bot._conv_list.get_all():
+            self.update(Conversation, source="init", automatic_save=False)
  
     def load_from_memory(self):
         if self.bot.memory.exists(['convmem']):
             convs = self.bot.memory.get_by_path(['convmem'])
-            logging.info("conversation_memory(): loading from memory {}".format(len(convs)))
+            logging.info("convmem(): loading conversations from memory {}".format(len(convs)))
             for convid in convs:
                 self.catalog[convid] = convs[convid]
 
@@ -103,9 +107,104 @@ class conversation_memory:
                 if "history" not in self.catalog[convid]:
                     self.catalog[convid]["history"] = True
 
+                if "participants" not in self.catalog[convid]:
+                    self.catalog[convid]["participants"] = [ u[0][0] for u in self.catalog[convid]["users"] ]
+
+                """add to permanent user memory if the record is valid"""
+
+                if len(self.catalog[convid]["users"]) > 0:
+                    _added = []
+
+                    for _u in self.catalog[convid]["users"]:
+                        UserID = hangups.user.UserID(chat_id=_u[0][0], gaia_id=_u[0][1])
+                        try:
+                            User = self.bot._user_list._user_dict[UserID]
+                            results = self.store_user_memory(
+                                User, is_definitive=True, automatic_save=False)
+
+                        except KeyError:
+                            User = hangups.user.User(
+                                UserID,
+                                _u[1],
+                                None,
+                                None,
+                                [],
+                                False)
+
+                            results = self.store_user_memory(
+                                User, is_definitive=False, automatic_save=False)
+
+                        if results:
+                            _added.append((_u[0][0], _u[1]))
+
+                    if len(_added) > 0:
+                        logging.info("convmem(): users added when loading {}: {}".format(
+                            convid, _added))
+
+
     def save_to_memory(self):
         self.bot.memory.set_by_path(['convmem'], self.catalog)
         self.bot.memory.save()
+
+
+    def store_user_memory(self, User, automatic_save=True, is_definitive=False):
+        self.bot.initialise_memory(User.id_.chat_id, "user_data")
+
+        cached = False
+        if self.bot.memory.exists(["user_data", User.id_.chat_id, "_hangups"]):
+            cached = self.bot.memory.get_by_path(["user_data", User.id_.chat_id, "_hangups"])
+            if "is_definitive" in cached and cached["is_definitive"] and is_definitive == False:
+                logging.info("convmem: user {} skipped update {}".format(cached["full_name"], cached["chat_id"]))
+                return False
+
+        user_dict ={
+            "chat_id": User.id_.chat_id,
+            "gaia_id": User.id_.gaia_id,
+            "full_name": User.full_name,
+            "first_name": User.first_name,
+            "photo_url": User.photo_url,
+            "emails": User.emails,
+            "is_self": User.is_self,
+            "is_definitive": is_definitive }
+
+        changed = False
+        if cached:
+            for key in list(user_dict.keys()):
+                try:
+                    if key == "emails":
+                        if set(user_dict[key]) != set(cached[key]):
+                            logging.info("convmem: user email changed {} ({})".format(User.full_name, User.id_.chat_id))
+                            changed = True
+                            break
+                    else:
+                        if user_dict[key] != cached[key]:
+                            logging.info("convmem: user {} changed {} ({})".format(key, User.full_name, User.id_.chat_id))
+                            changed = True
+                            break
+
+                except KeyError as e:
+                    logging.info("convmem: user {} missing {} ({})".format(key, User.full_name, User.id_.chat_id))
+                    changed = True
+                    break
+        else:
+            logging.info("convmem: new user {} ({})".format(User.full_name, User.id_.chat_id))
+            changed = True
+
+        if changed:
+            user_dict["updated"] = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            self.bot.memory.set_by_path(["user_data", User.id_.chat_id, "_hangups"], user_dict)
+
+            if automatic_save:
+                self.save_to_memory()
+
+            logging.info("convmem: user {} updated {}".format(User.id_.chat_id, User.full_name))
+            return True
+
+        else:
+            logging.info("convmem: user {} unchanged {}".format(User.id_.chat_id, User.full_name))
+            return False
+
+
 
     def update(self, conv, source="unknown", automatic_save=True):
         conv_title = hangups_get_conv_name(conv)
@@ -124,6 +223,11 @@ class conversation_memory:
 
         """store the user list"""
         memory["users"] = [[[user.id_.chat_id, user.id_.gaia_id], user.full_name ] for user in conv.users if not user.is_self]
+
+        memory["participants"] = []
+        for User in conv.users:
+            memory["participants"].append(User.id_.chat_id)
+            self.store_user_memory(User, automatic_save, is_definitive=True)
 
         """store the conversation type: GROUP, ONE_TO_ONE"""
         if conv._conversation.type_ == hangups.schemas.ConversationType.GROUP:
@@ -147,19 +251,32 @@ class conversation_memory:
 
         if original:
             """existing tracked conversation"""
-            for key in ["title", "type", "history", "users"]:
-                if key == "users":
-                    """special processing for users list"""
-                    if (set([ (u[0][0], u[0][1], u[1]) for u in original["users"] ])
-                            != set([ (u[0][0], u[0][1], u[1]) for u in memory["users"] ])):
-                        logging.info("convmem: users changed {} ({})".format(conv_title, conv.id_))
-                        changed = True
-                        break
-                else:
-                    if original[key] != memory[key]:
-                        logging.info("convmem: {} changed {} ({})".format(key,  conv_title, conv.id_))
-                        changed = True
-                        break
+            for key in ["title", "type", "history", "users", "participants"]:
+                try:
+                    if key == "participants":
+                        if set(original["participants"]) != set(memory["participants"]):
+                            logging.info("convmem: participants changed {} ({})".format(conv_title, conv.id_))
+                            changed = True
+                            break
+
+                    elif key == "users":
+                        """special processing for users list"""
+                        if (set([ (u[0][0], u[0][1], u[1]) for u in original["users"] ])
+                                != set([ (u[0][0], u[0][1], u[1]) for u in memory["users"] ])):
+                            logging.info("convmem: users changed {} ({})".format(conv_title, conv.id_))
+                            changed = True
+                            break
+
+                    else:
+                        if original[key] != memory[key]:
+                            logging.info("convmem: {} changed {} ({})".format(key,  conv_title, conv.id_))
+                            changed = True
+                            break
+
+                except KeyError as e:
+                    logging.info("convmem: missing {} {} ({})".format(key,  conv_title, conv.id_))
+                    changed = True
+                    break
         else:
             """new conversation"""
             logging.info("convmem: new {} ({})".format(conv_title, conv.id_))
