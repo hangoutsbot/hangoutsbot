bbe0b29c184be1162a55d4e101b9676bf5e86ef8
null pointer <eol+temporary-2@invalidfile.name>
2015-08-04 11:21:01 +0800

    renamed for consistency with other core command modules

--- hangupsbot/hangupsbot.py
+++ hangupsbot/hangupsbot.py
@@ -635,7 +635,7 @@ class HangupsBot(object):
 
         plugins.load(self, "commands.basic")
         plugins.load(self, "commands.tagging")
-        plugins.load(self, "commands.permamem_diagnostics")
+        plugins.load(self, "commands.permamem")
         plugins.load_user_plugins(self)
 
         self._conv_list.on_event.add_observer(self._on_event)
