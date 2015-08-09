51080dce62c4688ea51b8522d3d24a829ea98bd2
null pointer <eol+temporary-2@invalidfile.name>
2015-08-09 18:15:05 +0800

    upgrade google script sink to modern BaseBotRequestHandler, closes #297 adds improved test script - accept url as argument

--- hangupsbot/tests/google-mocksend.py
+++ hangupsbot/tests/google-mocksend.py
@@ -1,8 +1,24 @@
-# use with the simpledemo sink which is part of sinks.generic.simpledemo package
+"""simple test for sinks.google.scripts.webhookReceiver
+
+usage: google-mocksend.py [-h] url
+
+positional arguments:
+  url         url to send the data
+
+optional arguments:
+  -h, --help  show this help message and exit
+"""
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="url to send the data")
+args = parser.parse_args()
+
 import json
 import requests
-url = 'https://127.0.0.1:8002/Ugy7L_jAnik6pPUczsZ4AaABAQ/'
-payload = {"message" : "HELLO FROM MOCK GOOGLE!!!"}
+from datetime import datetime
+
+payload = {"message" : "HELLO FROM **NOT** GOOGLE!!! DATE AND TIME:" + str(datetime.now())}
 headers = {'content-type': 'application/json'}
-r = requests.post(url, data = json.dumps(payload), headers = headers, verify=False)
+r = requests.post(args.url, data = json.dumps(payload), headers = headers, verify=False)
+
 print(r)
