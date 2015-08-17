998b79cc79d293757978731cb356bbd7a4a72c32
null pointer <eol+temporary-2@invalidfile.name>
2015-08-17 18:42:10 +0800

    tldr can be retrieved individually, closes #323

--- hangupsbot/plugins/tldr.py
+++ hangupsbot/plugins/tldr.py
@@ -10,8 +10,9 @@ def _initialise(bot):
 def tldr(bot, event, *args):
     """Adds a short message to a list saved for the conversation using:
     /bot tldr <message>
-    All TLDRs can be retrieved by /bot tldr (without any parameters)
+    All TLDRs can be retrieved by /bot tldr, single tldr with /bot <number>
     All TLDRs can be deleted using /bot tldr clear, single tldr with /bot clear <number>"""
+    parameters = list(args)
 
     if not bot.memory.exists(['tldr']):
         bot.memory.set_by_path(['tldr'], {})
@@ -21,33 +22,41 @@ def tldr(bot, event, *args):
 
     conv_tldr = bot.memory.get_by_path(['tldr', event.conv_id])
 
-    if not args:
-        # Display all messages
-        if len(conv_tldr) > 0:
-            html = []
-            html.append(_("<b>TL;DR ({}):</b>").format(len(conv_tldr)))
-            for i, timestamp in enumerate(sorted(conv_tldr, key=float)):
-                html.append(_("{}. {} <b>{} ago</b>").format( str(i+1),
+    display = False
+    if not parameters:
+        display = True
+    elif len(parameters) == 1 and parameters[0].isdigit():
+        display = int(parameters[0]) - 1
+
+    if display is not False:
+        # Display all messages or a specific message
+        html = []
+        for num, timestamp in enumerate(sorted(conv_tldr, key=float)):
+            if display is True or display == num:
+                html.append(_("{}. {} <b>{} ago</b>").format( str(num+1),
                                                              conv_tldr[timestamp],
                                                              _time_ago(float(timestamp)) ))
 
-            yield from bot.coro_send_message(event.conv_id, "<br />".join(html))
-
+        if len(html) == 0:
+            html.append(_("TL;DR not found"))
         else:
-            yield from bot.coro_send_message(event.conv_id, _("Nothing in TL;DR"))
+            html.insert(0, _("<b>TL;DR ({} stored):</b>").format(len(conv_tldr)))
+        message = _("<br />".join(html))
+
+        yield from bot.coro_send_message(event.conv_id, message)
 
         return
 
-    if "clear" in args[0]:
-        if len(args) >= 2 and args[1].isdigit():
+    if "clear" in parameters[0]:
+        if len(parameters) >= 2 and parameters[1].isdigit():
             sorted_keys = sorted(list(conv_tldr.keys()), key=float)
-            key_index = int(args[1]) - 1
+            key_index = int(parameters[1]) - 1
             if key_index < 0 or key_index >= len(sorted_keys):
-                message = _("TL;DR #{} not found").format(args[1])
+                message = _("TL;DR #{} not found").format(parameters[1])
             else:
                 popped_tldr = conv_tldr.pop(sorted_keys[key_index])
                 bot.memory.set_by_path(['tldr', event.conv_id], conv_tldr)
-                message = _('TL;DR #{} removed - "{}"').format(args[1], popped_tldr)
+                message = _('TL;DR #{} removed - "{}"').format(parameters[1], popped_tldr)
         else:
             bot.memory.set_by_path(['tldr', event.conv_id], {})
             message = _("All TL;DRs cleared")
@@ -55,7 +64,7 @@ def tldr(bot, event, *args):
         yield from bot.coro_send_message(event.conv_id, message)
 
     else:
-        tldr = ' '.join(args).replace("'", "").replace('"', '')
+        tldr = ' '.join(parameters).replace("'", "").replace('"', '')
 
         if tldr:
             # Add message to list
