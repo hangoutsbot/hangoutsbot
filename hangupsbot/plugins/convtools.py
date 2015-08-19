9a7259651b8fe3e8ab9f51460e616b07026395e1
null pointer <eol+temporary-2@invalidfile.name>
2015-08-19 18:06:51 +0800

    convtools plugin refactor: * remove authorisation token, use parameter "test", closes #334 * more informative output all around * add /bot kick command <optional conv id> <user id> <user id> ... * /bot kick, /bot refresh renames new chat with old chat title, closes #333 * /bot kick adds [DEFUNCT] to old group, disable with "quietly" * /bot refresh adds [DEFUNCT] to old group, disable with "quietly" or "norename"

--- hangupsbot/plugins/convtools.py
+++ hangupsbot/plugins/convtools.py
@@ -1,16 +1,17 @@
 import asyncio, logging, random, string
 
+import hangups
+
 import plugins
+
+from commands import command
 
 
 logger = logging.getLogger(__name__)
 
 
-_externals = { "authorisation": False }
-
-
 def _initialise(bot):
-    plugins.register_admin_command(["addme", "addusers", "createconversation", "refresh"])
+    plugins.register_admin_command(["addme", "addusers", "createconversation", "refresh", "kick"])
 
 
 def addusers(bot, event, *args):
@@ -71,81 +72,152 @@ def createconversation(bot, event, *args):
 def refresh(bot, event, *args):
     """refresh a chat
     Usage: /bot refresh
-    conversation <chat id>
-    [without <user id(s)>]
-    [add <user id(s)>] [quietly]"""
+    [conversation] <conversation id>
+    [without|remove <user ids, space-separated if more than one>]
+    [with|add <user id(s)>]
+    [quietly]
+    [norename]"""
     parameters = list(args)
 
-    if _externals["authorisation"] not in parameters:
-        _externals["authorisation"] = False
-        initiator_1on1 = yield from bot.get_1to1(event.user.id_.chat_id)
-        if initiator_1on1:
-            _externals["authorisation"] = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
-            yield from bot.coro_send_message(initiator_1on1, "<i>are you sure? execute the command again and append this key: {}</i>".format(_externals["authorisation"]))
+    test = False
+    quietly = False
+    source_conv = False
+    renameold = True
+    list_removed = []
+    list_added = []
+
+    state = ["conversation"]
+
+    for parameter in parameters:
+        if parameter == "remove" or parameter == "without":
+            state.append("removeuser")
+        elif parameter == "add" or parameter == "with":
+            state.append("adduser")
+        elif parameter == "conversation":
+            state.append("conversation")
+        elif parameter == "quietly":
+            quietly = True
+            renameold = False
+        elif parameter == "test":
+            test = True
+        elif parameter == "norename":
+            renameold = False
         else:
-            yield from bot.coro_send_message(event.conv_id, "<i>you must have a 1on1 with the bot first</i>")
-    else:
-        parameters.remove(_externals["authorisation"])
-        _externals["authorisation"] = False
+            if state[-1] == "adduser":
+                list_added.append(parameter)
+                if parameter in list_removed:
+                    list_removed.remove(parameter)
 
-        quietly = False
-        target_conv = False
-        list_remove = []
-        list_add = []
+            elif state[-1] == "removeuser":
+                list_removed.append(parameter)
+                if parameter in list_added:
+                    list_added.remove(parameter)
 
-        state = ["adduser"]
+            elif state[-1] == "conversation":
+                source_conv = parameter
 
-        logger.info("refresh: {}".format(parameters))
-
-        for parameter in parameters:
-            if parameter == "without":
-                state.append("removeuser")
-            elif parameter == "add":
-                state.append("adduser")
-            elif parameter == "conversation":
-                state.append("conversation")
-            elif parameter == "quietly":
-                quietly = True
             else:
-                if state[-1] == "adduser":
-                    list_add.append(parameter)
-                    if parameter in list_remove:
-                        list_remove.remove(parameter)
+                raise ValueError("UNKNOWN STATE: {}".format(state[-1]))
 
-                elif state[-1] == "removeuser":
-                    list_remove.append(parameter)
-                    if parameter in list_add:
-                        list_add.remove(parameter)
+    list_removed = list(set(list_removed))
 
-                elif state[-1] == "conversation":
-                    target_conv = parameter
-                    state.pop()
+    if not source_conv:
+        raise ValueError("conversation id not supplied")
 
-                else:
-                    raise ValueError("UNKNOWN STATE: {}".format(state[-1]))
+    if source_conv not in bot.conversations.catalog:
+        raise ValueError(_("conversation {} not found").format(source_conv))
 
-        list_remove = list(set(list_remove))
+    if bot.conversations.catalog[source_conv]["type"] != "GROUP":
+        raise ValueError(_("conversation {} is not a GROUP").format(source_conv))
 
-        if not target_conv:
-            logger.error("REFRESH: conversation must be supplied")
-            return
+    new_title = bot.conversations.get_name(source_conv)
+    old_title = _("[DEFUNCT] {}".format(new_title))
 
-        list_all_users = bot.get_users_in_conversation(target_conv)
-        logger.info("refresh: conversation {} has {} users".format(target_conv, len(list_all_users)))
+    text_removed_users = []
+    list_all_users = bot.get_users_in_conversation(source_conv)
+    for u in list_all_users:
+        if u.id_.chat_id not in list_removed:
+            list_added.append(u.id_.chat_id)
+        else:
+            hangups_user = bot.get_hangups_user(u.id_.chat_id)
+            text_removed_users.append("<pre>{}</pre> ({})".format(hangups_user.full_name, u.id_.chat_id))
 
-        for u in list_all_users:
-            if not u.id_.chat_id in list_remove:
-                list_add.append(u.id_.chat_id)
+    list_added = list(set(list_added))
 
-        list_add = list(set(list_add))
+    logger.debug("refresh: from conversation {} removed {} added {}".format(source_conv, len(list_removed), len(list_added)))
 
-        logger.info("refresh: from conversation {} removed {} added {} {}".format(target_conv, list_remove, len(list_add), list_add))
-
-        if len(list_add) > 1:
-            response = yield from bot._client.createconversation(list_add)
-
+    if test:
+        yield from bot.coro_send_message(event.conv_id,
+                                         _("<b>refresh:</b> {}<br />"
+                                           "<b>rename old: {}</b><br />"
+                                           "<b>removed {}:</b> {}<br />"
+                                           "<b>added {}:</b> {}").format(source_conv,
+                                                                         old_title if renameold else _("<em>unchanged</em>"),
+                                                                         len(text_removed_users),
+                                                                         ", ".join(text_removed_users) or _("<em>none</em>"),
+                                                                         len(list_added),
+                                                                         " ".join(list_added) or _("<em>none</em>")))
+    else:
+        if len(list_added) > 1:
+            response = yield from bot._client.createconversation(list_added, force_group=True)
             new_conversation_id = response['conversation']['id']['id']
             yield from bot.coro_send_message(new_conversation_id, _("<i>group refreshed</i><br />"))
-            yield from bot.coro_send_message(event.conv_id, _("<b>group obsoleted</b>"))
+            yield from asyncio.sleep(1)
+            yield from bot._client.setchatname(new_conversation_id, new_title)
+
+            if renameold:
+                yield from bot._client.setchatname(source_conv, old_title)
+
             if not quietly:
-                yield from bot.coro_send_message(target_conv, _("<b>group obsoleted</b>"))
+                yield from bot.coro_send_message(source_conv, _("<i>group has been obsoleted</i>"))
+
+            yield from bot.coro_send_message( event.conv_id,
+                                              _("refreshed: <b><pre>{}</pre></b> (original id: <pre>{}</pre>).<br />"
+                                                "new conversation id: <b><pre>{}</pre></b>.<br />"
+                                                "removed {}: {}").format( new_title,
+                                                                          source_conv,
+                                                                          new_conversation_id,
+                                                                          len(text_removed_users),
+                                                                          ", ".join(text_removed_users) or _("<em>none</em>") ))
+
+        else:
+            yield from bot.coro_send_message(event.conv_id, _("<b>nobody to add in the new conversation</b>"))
+
+
+def kick(bot, event, *args):
+    """kick users from a conversation
+    Usage: /bot kick
+    [<optional conversation id, current if not specified>]
+    [<user ids, space-separated if more than one>]
+    [quietly]"""
+    parameters = list(args)
+
+    source_conv = event.conv_id
+    remove = []
+    test = False
+    quietly = False
+
+    for parameter in parameters:
+        if parameter in bot.conversations.catalog:
+            source_conv = parameter
+        elif parameter in bot.conversations.catalog[source_conv]["participants"]:
+            remove.append(parameter)
+        elif parameter == "test":
+            test = True
+        elif parameter == "quietly":
+            quietly = True
+        else:
+            raise ValueError(_("supply optional conversation id and valid user ids to kick"))
+
+    if len(remove) <= 0:
+        raise ValueError(_("supply at least one valid user id to kick"))
+
+    arguments = ["refresh", source_conv, "without"] + remove
+
+    if test:
+        arguments.append("test")
+
+    if quietly:
+        arguments.append("quietly")
+
+    yield from command.run(bot, event, *arguments)
