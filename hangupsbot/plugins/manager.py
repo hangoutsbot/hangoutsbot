fad2102e7a6677f0cf6dc93f962eb7c3bbaf6769
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-11-22 23:56:46 -0500

    added UNSTABLE checkbox functionality

--- hangupsbot/plugins/manager.py
+++ hangupsbot/plugins/manager.py
@@ -11,7 +11,7 @@ def _initialise(bot):
 
 def _start_manager(bot):
     """will host a basic plugin manager at localhost:9090"""
-    cherrypy.config.update({'server.socket_port': 9093})
+    cherrypy.config.update({'server.socket_port': 9090})
     # Start server
     cherrypy.tree.mount(PluginManager(bot), '/')
     cherrypy.engine.start()
@@ -27,13 +27,30 @@ class PluginManager(object):
     def plugins(self):
         all_plugins = plugins.retrieve_all_plugins()
         loaded_plugins = plugins.get_configured_plugins(self._bot)
-        html = "<b>Loaded Plugins</b><br>"
+        html = """<html>
+        <head></head>
+          <body>
+            <b>Loaded Plugins</b><br>
+            <form method="get" action="plugins_submit">"""
         for plugin in all_plugins:
             if plugin in loaded_plugins:
                 checked = " checked"
             else:
                 checked = ""
             html += "<input type=\"checkbox\" name=\"plugin\" value=\"{0}\"{1}> {0}<br>".format(plugin, checked)
+
+        html += """
+              <button type="submit">Save</button>
+            </form>
+          </body>
+        </html>"""
         return html
+
+    @cherrypy.expose
+    def plugins_submit(self, plugin):
+        print("Plugins: {}".format(plugin))
+        self._bot.config.set_by_path(["plugins"], plugin)
+        self._bot.config.load()
+        return "Plugins successfully updated!"
 
     index.exposed = True
