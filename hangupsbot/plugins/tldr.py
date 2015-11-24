4c16ae3c540d9866d9956f56be4a290731e1e0b2
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2015-11-24 14:46:04 -0500

    Updated help message for tldr

--- hangupsbot/plugins/tldr.py
+++ hangupsbot/plugins/tldr.py
@@ -11,7 +11,8 @@ def tldr(bot, event, *args):
     """Adds a short message to a list saved for the conversation using:
     /bot tldr <message>
     All TLDRs can be retrieved by /bot tldr, single tldr with /bot <number>
-    All TLDRs can be deleted using /bot tldr clear, single tldr with /bot clear <number>"""
+    All TLDRs can be deleted using /bot tldr clear, single tldr with /bot clear <number>
+    Single TLDRs can be edited using /bot tldr edit <number> <new_message>"""
     parameters = list(args)
 
     if not bot.memory.exists(['tldr']):
