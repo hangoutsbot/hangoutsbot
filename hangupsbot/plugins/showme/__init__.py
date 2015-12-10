b4c4178e1dc1200405da42db68603da39d3f932f
Daniel Casner <daniel@anki.com>
2015-12-09 16:39:05 -0800

    Publishability improvements: * Move showme plugin into a directory for modularity * Add license * Improve help text * Add readme

--- hangupsbot/plugins/showme/__init__.py
+++ hangupsbot/plugins/showme/__init__.py
@@ -4,6 +4,33 @@ sent them to the user.
 
 Config must specify aliases and urls which should include any nessisary auth.
 """
+__LICENSE__ = """
+The BSD License
+Copyright (c) 2015, Daniel Casner
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+__author__ = "Daniel Casner <www.artificelab.com>"
 
 import time
 import aiohttp, asyncio, io, logging
@@ -23,9 +50,11 @@ def showme(bot, event, *args):
     if not len(args):
         yield from bot.coro_send_message(event.conv, _("Show you what?"))
     elif args[0].lower() in ('sources','help'):
-        html = "My sources are:<br />"
+        html = """Retrieve images from showme sources by saying<br />
+/bot showme SOURCE<br />
+My sources are:<br />"""
         for name in sources.keys():
-            html += "* {}<br />".format(name)
+            html += " * {}<br />".format(name)
         yield from bot.coro_send_message(event.conv, _(html))
     elif not args[0] in sources:
         yield from bot.coro_send_message(event.conv, _("I don't know a \"{}\", try help".format(args[0])))
