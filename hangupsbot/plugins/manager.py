cf04cb1a6a2b2584d1ea268d8fb3617729098e4d
gurumadmat <gurumadmat@gmail.com>
2015-12-19 14:49:33 +0100

    Update manager.py
    Update with leave conversation option, but it doesn't work yet.

--- hangupsbot/plugins/manager.py
+++ hangupsbot/plugins/manager.py
@@ -3,24 +3,29 @@ import asyncio, logging, json
 import plugins
 
 import cherrypy
+import os
+from commands import command
 
 logger = logging.getLogger(__name__)
 
 def _initialise(bot):
     plugins.register_handler(_start_manager(bot))
+    
 
 def _start_manager(bot):
     """will host a basic plugin manager"""
 
-    manager = bot.get_config_option('manager')
+    port = 9090
+    path   = os.path.abspath(os.path.dirname(__file__))
+    config = {
 
-    try:
-        port = manager["port"]
-    except (KeyError, TypeError) as e:
-        logger.error("config.manager missing keyword", e)
-        logger.info("using default port for manager.py: 9090")
-        port = 9090
-
+      '/manager' : {
+        'tools.staticdir.on'            : True,
+        'tools.staticdir.dir'           : os.path.join(path, 'manager')
+        
+      }
+    }
+    cherrypy.config.update(config)
     cherrypy.config.update({'server.socket_port': port})
     cherrypy.server.socket_host = '0.0.0.0' # cherrypy will listen on any ip
     # Start server
@@ -28,6 +33,7 @@ def _start_manager(bot):
     cherrypy.engine.start()
 
 class PluginManager(object):
+    
     def __init__(self, bot):
         self._bot = bot
 
@@ -40,6 +46,7 @@ class PluginManager(object):
     def plugins(self):
         all_plugins = plugins.retrieve_all_plugins()
         loaded_plugins = plugins.get_configured_plugins(self._bot)
+        
         html = """<html>
         <head></head>
           <body>
@@ -79,7 +86,7 @@ class PluginManager(object):
             <td style=\"vertical-align:top\">
             """
         for convid, convdata in group_conversations.items():
-            html += "<input style=\"width: 100%  ;\" type=\"text\" name=\"conv\" value=\"{0}\"> <br>".format(convdata["title"])
+            html += "<input style=\"width: 95%  ;\" type=\"text\" name=\"conv\" value=\"{0}\"><img onclick=\"location.href='conv_leave?id={1}';\" height=\"16\" width=\"16\" alt=\"Leave conversation\" src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABAUlEQVRYR+2XQU7DQAxF/U/CEYC9R3Azyg3gJj1CqlhRdoQbhBvAOok+MiqVqJIMQ0tXHimLyOPxy1tk9CGFK6X0TvJFRCqSPcnXpmm6wmMO27HUqKr3InJ3XAewmesh6RC9iHQAqmEY3tq29ffVtQawAfCQOyBTd1sO9FzX9XZubxaA5M51fzf/BYrko5nNmvsNwI/mlBJLrQRAGAgDYSAMnN2AX8FmdqOqPYCr3K/5PwA6M7tVVb/pri8OkBt4XD+7gQAIA2EgDISBSxnwbOhPZWaHZFQYTD720eypOJotfeUSgF/RADwte0itxnHsTwqnKwBfgdOH+MBpmrpT4vknX/8GP+z8WWcAAAAASUVORK5CYII=\"/> <br>".format(convdata["title"],convid)
 
         html += "</td><td style=\"vertical-align:top\>"
 
@@ -103,4 +110,17 @@ class PluginManager(object):
         self._bot.config.load()
         return "Plugins successfully updated!<br><a href='/'>Back to bot configuration</a>"
 
+    @cherrypy.expose
+
+    def conv_leave(self, id):
+        
+        # workaround for missing event
+        #event = {'conv_id' : "blob" }
+       
+        
+        res = yield from self._bot._client.removeuser(id)
+        return "Left  !<br><a href='/'>Back to bot configuration</a>"
+
+
     index.exposed = True
+
