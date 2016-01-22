f70f83666f7d97266fc5d6ee4f5b196ce047d0a8
Dean Vigoren <vigorator@gmail.com>
2016-01-22 13:05:50 -0700

    Added support for TAF lookup Improved the exception handling to look for parse error exceptions only

--- hangupsbot/plugins/metar.py
+++ hangupsbot/plugins/metar.py
@@ -6,8 +6,19 @@ from xml.etree import ElementTree
 logger = logging.getLogger(__name__)
 
 def _initialize(bot):
-    plugins.register_user_command(['metar'])
+    plugins.register_user_command(['metar','taf'])
 
+def _api_lookup(type, iaco):
+    api_url = "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource={0}s&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString={1}".format(type, iaco)
+    r= requests.get(api_url)
+    try:
+        root = ElementTree.fromstring(r.content)        
+        raw = root.findall('data/{}/raw_text'.format(type))
+    except ElementTree.ParseError as e:
+        logger.info("METAR Error: {}".format(e))
+        return None
+    return raw
+    
 def metar(bot, event, *args):
     """
     Looks up the most recent METAR weather report for the supplied ICAO airport code.
@@ -20,17 +31,33 @@ def metar(bot, event, *args):
         yield from bot.coro_send_message(event.conv_id, "You need to enter the ICAO airport code you wish the look up, https://en.wikipedia.org/wiki/International_Civil_Aviation_Organization_airport_code .")
         return
     
-    api_url = "http://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true&stationString="+code
-    r= requests.get(api_url)
+    data = _api_lookup('METAR',code)
     
-    try:
-        root = ElementTree.fromstring(r.content)        
-        raw = root.findall('data/METAR/raw_text')
-        if not raw or len(raw) == 0:
-            yield from bot.coro_send_message(event.conv_id, "The response did not contain METAR information, check the ICAO airport code and try again.")
-            return
-        yield from bot.coro_send_message(event.conv_id, raw[0].text)        
-        
-    except Exception as e:
-        logger.info("METAR Error: {}".format(e))
+    if data is None:
         yield from bot.coro_send_message(event.conv_id, "There was an error retrieving the METAR information.")
+    elif not data or len(data) == 0:
+        yield from bot.coro_send_message(event.conv_id, "The response did not contain METAR information, check the ICAO airport code and try again.")
+    else:
+        yield from bot.coro_send_message(event.conv_id, data[0].text)
+
+def taf(bot, event, *args):
+    """
+    Looks up the most recent TAF weather forecast for the supplied ICAO airport code.
+        <b>/bot taf <ICAO airport code></b>
+    ICAO Airport Codes: https://en.wikipedia.org/wiki/International_Civil_Aviation_Organization_airport_code
+    TAF source: http://aviationweather.gov
+    """
+    
+    code = ''.join(args).strip()
+    if not code:
+        yield from bot.coro_send_message(event.conv_id, "You need to enter the ICAO airport code you wish the look up, https://en.wikipedia.org/wiki/International_Civil_Aviation_Organization_airport_code .")
+        return
+    
+    data = _api_lookup('TAF',code)
+    
+    if data is None:
+        yield from bot.coro_send_message(event.conv_id, "There was an error retrieving the TAF information.")
+    elif not data or len(data) == 0:
+        yield from bot.coro_send_message(event.conv_id, "The response did not contain TAF information, check the ICAO airport code and try again.")
+    else:
+        yield from bot.coro_send_message(event.conv_id, data[0].text)
