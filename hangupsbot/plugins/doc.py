1bfc0f4492e33e3af65be1e088713381a10087dd
cd334 <czigany.daniel@gmail.com>
2016-03-19 21:16:47 +0100

    Update and rename doc.py to botdoc.py

--- hangupsbot/plugins/doc.py
+++
@@ -1,8 +0,0 @@
-import plugins, asyncio
-
-def _initialise(bot):
-   plugins.register_user_command(["doc"])
-
-@asyncio.coroutine
-def doc(bot, event, *args):
-   yield from bot.coro_send_message(event.conv,'<a href="adrress">https://goo.gl/xxxx</a>')
