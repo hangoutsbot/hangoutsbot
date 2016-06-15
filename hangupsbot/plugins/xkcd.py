13f9b39d2d6d0d46223fb9d76c7daf1072f2a26c
fkloft <felix.kloft+github@gmail.com>
2016-06-16 00:58:01 +0200

    new command: /bot xkcd prints the latest comic
    also, cache the comics and prevent re-uploading the images

--- hangupsbot/plugins/xkcd.py
+++ hangupsbot/plugins/xkcd.py
@@ -10,7 +10,10 @@ import json
 import os.path
 import re
 
+_cache = {}
+
 def _initialise():
+    plugins.register_user_command(["xkcd"])
     plugins.register_handler(_watch_xkcd_link, type="message")
 
 regexps = (
@@ -18,6 +21,18 @@ regexps = (
     "https?://(?:www\.)?explainxkcd.com/wiki/index\.php(?:/|\?title=)([0-9]+)(?:[^0-9]|$)",
     "(?:\s|^)xkcd\s+(?:#\s*)?([0-9]+)(?:\s|$)",
 )
+
+@asyncio.coroutine
+def xkcd(bot, event, *args):
+    """show latest xkcd comic"""
+    
+    if args == ("clear", ):
+        _cache.clear()
+    
+    if len(args):
+        # will be handled by the message handler
+        return
+    yield from _print_comic(bot, event)
 
 @asyncio.coroutine
 def _watch_xkcd_link(bot, event, command):
@@ -31,31 +46,50 @@ def _watch_xkcd_link(bot, event, command):
             continue
         
         num = match.group(1)
-        request = yield from aiohttp.request('get', 'https://xkcd.com/%s/info.0.json' % num)
+        yield from _print_comic(bot, event, num)
+        return # only one match per message
+
+def _print_comic(bot, event, num=None):
+    if num:
+        num = int(num)
+        url = 'https://xkcd.com/%d/info.0.json' % num
+    else:
+        num = None
+        url = 'https://xkcd.com/info.0.json'
+    
+    if num in _cache:
+        info = _cache[num]
+    else:
+        request = yield from aiohttp.request('get', url)
         raw = yield from request.read()
         info = json.loads(raw.decode())
         
-        filename = os.path.basename(info["img"])
-        request = yield from aiohttp.request('get', info["img"])
-        raw = yield from request.read()
-        image_data = io.BytesIO(raw)
-        image_id = yield from bot._client.upload_image(image_data, filename=filename)
-        
-        context = {
-            "parser": False,
-        }
-        
-        msg1 = [
-            ChatMessageSegment("xkcd #%s: " % num),
-            ChatMessageSegment(info["title"], is_bold=True),
-        ]
-        msg2 = [
-            ChatMessageSegment(info["alt"]),
-        ] + ChatMessageSegment.from_str('<br/>- <i><a href="https://xkcd.com/%s">CC-BY-SA xkcd</a></i>' % num)
-        if "link" in info and info["link"]:
-            msg2.extend(ChatMessageSegment.from_str("<br/>* see also %s" % info["link"]))
-        
-        yield from bot.coro_send_message(event.conv.id_, msg1, context)
-        yield from bot.coro_send_message(event.conv.id_, msg2, context, image_id=image_id) # image appears above text, so order is [msg1, image, msg2]
-        
-        return # only one match per message
+        if info['num'] in _cache:
+            info = _cache[info['num']]
+        else:
+            filename = os.path.basename(info["img"])
+            request = yield from aiohttp.request('get', info["img"])
+            raw = yield from request.read()
+            image_data = io.BytesIO(raw)
+            info['image_id'] = yield from bot._client.upload_image(image_data, filename=filename)
+            _cache[info['num']] = info
+    
+    image_id = info['image_id']
+    
+    context = {
+        "parser": False,
+    }
+    
+    msg1 = [
+        ChatMessageSegment("xkcd #%s: " % info['num']),
+        ChatMessageSegment(info["title"], is_bold=True),
+    ]
+    msg2 = [
+        ChatMessageSegment(info["alt"]),
+    ] + ChatMessageSegment.from_str('<br/>- <i><a href="https://xkcd.com/%s">CC-BY-SA xkcd</a></i>' % info['num'])
+    if "link" in info and info["link"]:
+        msg2.extend(ChatMessageSegment.from_str("<br/>* see also %s" % info["link"]))
+    
+    yield from bot.coro_send_message(event.conv.id_, msg1, context)
+    yield from bot.coro_send_message(event.conv.id_, msg2, context, image_id=image_id) # image appears above text, so order is [msg1, image, msg2]
+
