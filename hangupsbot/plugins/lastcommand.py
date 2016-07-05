96a9c5fc1cfd56c460f28328492620505ef5e508
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-07-05 23:23:39 +0800

    removed c from command register

--- hangupsbot/plugins/lastcommand.py
+++ hangupsbot/plugins/lastcommand.py
@@ -6,7 +6,6 @@ from commands import command
 def _initialise(bot):
     plugins.register_handler(_handle_command, "message")
     plugins.register_admin_command(["lastcommand"])
-    plugins.register_user_command(["c"])
 
     # use bash-type command !! for recalling last command
     command.register(c, admin=False, final=True, name='!!')
@@ -19,14 +18,14 @@ def _handle_command(bot, event, command):
     except Exception as e:
         return
 
-    if line_args[0].lower() in bot._handlers.bot_command and (line_args[1].lower() != 'c' and line_args[1].lower() != '!!'):
+    if line_args[0].lower() in bot._handlers.bot_command and line_args[1].lower() != '!!':
         bot.user_memory_set(event.user.id_.chat_id, 'lastcommand', ' '.join(line_args[1:]))
 
 def c(bot, event):
     if bot.memory.exists(["user_data", event.user.id_.chat_id, 'lastcommand']):
         lastcommand = bot.user_memory_get(event.user.id_.chat_id, 'lastcommand')
         yield from bot.coro_send_message(event.conv, '<b>Last command :<br>' + lastcommand , context={ "parser": True })
-        if not (lastcommand is 'c' and lastcommand is '!!'):
+        if not lastcommand is '!!':
             yield from command.run(bot, event, *lastcommand.split())
     else:
         yield from bot.coro_send_message(event.conv, 'You have no previous command.'  , context={ "parser": True })
