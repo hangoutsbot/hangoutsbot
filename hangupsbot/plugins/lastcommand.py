3f4de1b927228284641455872e6106d8131964e4
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-07-05 23:21:05 +0800

    added experimental bash-type command

--- hangupsbot/plugins/lastcommand.py
+++ hangupsbot/plugins/lastcommand.py
@@ -1,44 +1,36 @@
-import hangups
-from hangups.ui.utils import get_conv_name
-import asyncio
 import plugins
 import shlex
 
 from commands import command
 
-def _initialise(Handlers, bot=None):
-    """
-    first, you can setup stuff, initialise variables and whatever else!
-    """
+def _initialise(bot):
     plugins.register_handler(_handle_command, "message")
     plugins.register_admin_command(["lastcommand"])
     plugins.register_user_command(["c"])
 
-    # above command is available to all users
-    # Handlers.register_admin_command() if command(s) only available to admins
-    return [] # always a blank list
+    # use bash-type command !! for recalling last command
+    command.register(c, admin=False, final=True, name='!!')
+    return []
 
 def _handle_command(bot, event, command):
     # Parse message
-    event.text = event.text.replace(u'\xa0', u' ') # convert non-breaking space in Latin1 (ISO 8859-1)
     try:
         line_args = shlex.split(event.text, posix=False)
     except Exception as e:
         return
 
-    if line_args[0].lower() in bot._handlers.bot_command and line_args[1].lower() != 'c':
+    if line_args[0].lower() in bot._handlers.bot_command and (line_args[1].lower() != 'c' and line_args[1].lower() != '!!'):
         bot.user_memory_set(event.user.id_.chat_id, 'lastcommand', ' '.join(line_args[1:]))
 
 def c(bot, event):
     if bot.memory.exists(["user_data", event.user.id_.chat_id, 'lastcommand']):
         lastcommand = bot.user_memory_get(event.user.id_.chat_id, 'lastcommand')
         yield from bot.coro_send_message(event.conv, '<b>Last command :<br>' + lastcommand , context={ "parser": True })
-        if not lastcommand is 'c':
+        if not (lastcommand is 'c' and lastcommand is '!!'):
             yield from command.run(bot, event, *lastcommand.split())
     else:
         yield from bot.coro_send_message(event.conv, 'You have no previous command.'  , context={ "parser": True })
 
-@asyncio.coroutine
 def lastcommand(bot, event, *args):
 	if not args:
 		yield from bot.coro_send_message(event.conv_id, "Please provide a userid!")
