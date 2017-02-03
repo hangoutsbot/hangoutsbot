37d44c8c41fcf9a42e9d742bec69835bc3a945a6
PrinterElf <PrinterElf@users.noreply.github.com>
2017-02-03 12:51:29 +0000

    Correct error made in previous commit.

--- hangupsbot/plugins/tldr.py
+++ hangupsbot/plugins/tldr.py
@@ -64,7 +64,7 @@ def tldr(bot, event, *args):
 
     message, display = tldr_base(bot, event.conv_id, list(args))
 
-    if display is True and tldr_echo_options[tldr_echo] is 'PM':
+    if display is True and tldr_echo is 'PM':
         yield from bot.coro_send_to_user_and_conversation(event.user.id_.chat_id, event.conv_id, message, ("<i>{}, I've sent you the info in a PM</i>").format(event.user.full_name))
     else:
         yield from bot.coro_send_message(event.conv_id, message)
