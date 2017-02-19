c7d143123dd867492606db3234984b4b7b8449e6
das7pad <das7pad@outlook.com>
2017-02-19 16:36:28 +0100

    Watermark only on recent activity
    rewrite the queue, sleep during the iteration for admins and groups seperately, propper backwards compatibility, store permanent failiures

--- hangupsbot/plugins/botalive.py
+++ hangupsbot/plugins/botalive.py
@@ -1,142 +1,171 @@
-import asyncio, datetime, logging, time
+"""plugin to watermark conversations periodically determined by config entry"""
 
+import asyncio
+import datetime
 import hangups
-
+import logging
 import plugins
-
-import threadmanager
+import random
 
 logger = logging.getLogger(__name__)
-
-
-internal = {}
-
 
 def _initialise(bot):
     config_botalive = bot.get_config_option("botalive") or {}
     if not config_botalive:
         return
 
-    _new_config = {}
+    if not bot.memory.exists(["conv_data"]):
+        # should not come to this, but create it once as we need to store data for each conv in it
+        bot.memory.set_by_path(["conv_data"], {})
+        bot.memory.save()
+
+    # backwards compatibility
     if isinstance(config_botalive, list):
+        _new_config = {}
         if "admins" in config_botalive:
             _new_config["admins"] = 900
         if "groups" in config_botalive:
             _new_config["groups"] = 10800
+        bot.config.set_by_path(["botalive"], _new_config)
+        bot.config.save()
         config_botalive = _new_config
 
-    if "admin" in config_botalive and config_botalive["admin"] < 60:
-        config_botalive["admin"] = 60
-    if "groups" in config_botalive and config_botalive["groups"] < 60:
-        config_botalive["groups"] = 60
-
-    logger.info("timing {}".format(config_botalive))
-
-    plugins.start_asyncio_task(_periodic_watermark_update, config_botalive)
-
-
-@asyncio.coroutine
-def _periodic_watermark_update(bot, config_botalive):
-    """runs in a separate thread - to prevent the processor from being
-    consumed entirely, we sleep for 5 seconds on each loop iteration"""
-
-    last_run = [0, 0]
+    if "admins" not in config_botalive and "groups" not in config_botalive:
+        return
 
     watermarkUpdater = watermark_updater(bot)
 
-    while True:
-        timestamp = time.time()
+    if "admins" in config_botalive:
+        if config_botalive["admins"] < 60:
+            config_botalive["admins"] = 60
+        plugins.start_asyncio_task(_periodic_watermark_queue_admins, watermarkUpdater, config_botalive["admins"])
 
-        yield from asyncio.sleep(5)
+    if "groups" in config_botalive:
+        if config_botalive["groups"] < 60:
+            config_botalive["groups"] = 60
+        plugins.start_asyncio_task(_periodic_watermark_queue_groups, watermarkUpdater, config_botalive["groups"])
 
-        """every 15 minutes: update watermark of global admin 1-on-1s"""
-        if "admins" in config_botalive and timestamp - last_run[0] > config_botalive["admins"]:
-            admins = bot.get_config_option('admins')
-            for admin in admins:
-                if bot.memory.exists(["user_data", admin, "1on1"]):
-                    conv_id = bot.memory.get_by_path(["user_data", admin, "1on1"])
-                    watermarkUpdater.add(conv_id)
-            yield from watermarkUpdater.start()
-            last_run[0] = timestamp
+    logger.info("timing {}".format(config_botalive))
 
-        """every 3 hours: update watermark of all groups"""
-        if "groups" in config_botalive and timestamp - last_run[1] > config_botalive["groups"]:
-            for conv_id, conv_data in bot.conversations.get().items():
-                if conv_data["type"] == "GROUP":
-                    watermarkUpdater.add(conv_id)
-            yield from watermarkUpdater.start()
-            last_run[1] = timestamp
+    watch_event_types = [
+        "message",
+        "membership",
+        "rename"
+        ]
+    for event_type in watch_event_types:
+        plugins.register_handler(_log_message, event_type)
 
+def _log_message(bot, event, command):
+    """log time to conv_data of event conv"""
 
-class watermark_updater:
-    """implement a simple queue to update the watermarks sequentially instead of all-at-once
+    conv_id = str(event.conv_id)
+    if not bot.memory.exists(["conv_data", conv_id]):
+        bot.memory.set_by_path(["conv_data", conv_id], {})
+        bot.memory.save()
+    bot.memory.set_by_path(["conv_data", conv_id, "botalive"], datetime.datetime.now().timestamp())
+    # not worth a dump to disk, skip bot.memory.save()
 
-    usage: .add("<conv id>") as many conversation ids as you want, then call .start()
-
-    if a hangups exception is raised, log the exception and output to console
+@asyncio.coroutine
+def _periodic_watermark_queue_admins(bot, watermarkUpdater, config_admins):
+    """
+    add conv_ids of 1on1s with bot admins to the queue
+    to prevent the processor from being consumed entirely, we sleep until the next run or 5 sec
     """
 
-    bot = None
+    last_run = datetime.datetime.now().timestamp()
 
-    queue = []
-    busy = False
+    while True:
+        timestamp = datetime.datetime.now().timestamp()
+        yield from asyncio.sleep(max(5, last_run - timestamp + config_admins))
 
-    _current_convid = False
-    _critical_errors = 0 # track repeated errors during asyncio.async call, resets when batch finished
+        bot_admin_ids = bot.get_config_option('admins')
+        for admin in bot_admin_ids:
+            if bot.memory.exists(["user_data", admin, "1on1"]):
+                conv_id = bot.memory.get_by_path(["user_data", admin, "1on1"])
+                watermarkUpdater.add(conv_id)
+
+        last_run = datetime.datetime.now().timestamp()
+        yield from watermarkUpdater.start()
+
+@asyncio.coroutine
+def _periodic_watermark_queue_groups(bot, watermarkUpdater, config_groups):
+    """
+    add conv_ids of all groups to the queue
+    to prevent the processor from being consumed entirely, we sleep until the next run or 5 sec
+    """
+
+    last_run = datetime.datetime.now().timestamp()
+
+    while True:
+        timestamp = datetime.datetime.now().timestamp()
+        yield from asyncio.sleep(max(5, last_run - timestamp + config_groups))
+
+        for conv_id, conv_data in bot.conversations.get().items():
+            if conv_data["type"] == "GROUP" and bot.memory.exists(["conv_data", conv_id, "botalive"]):
+                if last_run < bot.memory.get_by_path(["conv_data", conv_id, "botalive"]):
+                    watermarkUpdater.add(conv_id)
+
+        last_run = datetime.datetime.now().timestamp()
+        yield from watermarkUpdater.start()
+
+class watermark_updater:
+    """implement a queue to update the watermarks sequentially instead of all-at-once
+
+    usage:
+    .add("<conv id>") as many conversation ids as you want
+    .start() will start processing to queue
+
+    if a hangups exception is raised, log the exception and output to console
+    to prevent the processor from being consumed entirely and to not act too much as a bot,
+     we sleep 5-10sec after each watermark update"""
 
     def __init__(self, bot):
         self.bot = bot
+        self.running = False
+
+        self.queue = set()
+        self.failed = dict() # track errors
+        self.failed_permanent = set() # track conv_ids that failed 5 times
 
     def add(self, conv_id):
-        if conv_id not in self.queue:
-            self.queue.append(conv_id)
+        if conv_id not in self.failed_permanent:
+            self.queue.add(conv_id)
 
-
-    @asyncio.coroutine
     def start(self):
-        if self.busy:
+        if self.running or not len(self.queue):
             return
-
-        self.busy = True
-
+        self.running = True
         yield from self.update_next_conversation()
-
 
     @asyncio.coroutine
     def update_next_conversation(self):
-        if len(self.queue) > 0:
-            conv_id = self.queue.pop(0)
-
-        else:
-            self.busy = False
-            self._current_convid = False
-            self._critical_errors = 0
-
-            logger.debug("no more conversations to watermark")
+        try:
+            conv_id = self.queue.pop()
+        except:
+            self.running = False
             return
 
         logger.info("watermarking {}".format(conv_id))
 
-        self._current_convid = conv_id
-
         try:
-            yield from self.bot._client.updatewatermark( self._current_convid, 
-                                                         datetime.datetime.fromtimestamp(time.time()))
+            yield from self.bot._client.updatewatermark(
+                conv_id,
+                datetime.datetime.now()
+                )
+            self.failed.pop(conv_id, None)
 
-            if self._critical_errors > 0:
-                self._critical_errors = self._critical_errors - 1
+        except:
+            self.failed[conv_id] = self.failed.get(conv_id, 0) + 1
 
-        except Exception as e:
-            self._critical_errors = self._critical_errors + 1
-
-            if self._critical_errors > max(10, len(self.queue) * 2):
-                logger.error("critical error threshold reached, clearing queue")
-                self.queue = []
-
+            if self.failed[conv_id] > 5:
+                self.failed_permanent.add(conv_id)
+                logger.error("critical error threshold reached for {}".format(conv_id))
             else:
-                logger.exception("WATERMARK FAILED FOR {}".format(self._current_convid))
-                self.add(self._current_convid)
+                logger.exception("WATERMARK FAILED FOR {}".format(conv_id))
 
-                yield from asyncio.sleep(1)
+                # is the bot still in the conv
+                if conv_id in self.bot.conversations.get():
+                    self.add(conv_id)
 
+        yield from asyncio.sleep(random.randint(5, 10))
         yield from self.update_next_conversation()
