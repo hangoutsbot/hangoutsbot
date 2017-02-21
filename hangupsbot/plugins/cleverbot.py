78c86bad1ecc31f2d53d629c09dbad93635bdeab
Nick Young <nick.young@auckland.ac.nz>
2017-02-22 10:30:37 +1300

    make it so that you don't have to reload the plugin when you enter an api key, comment why cleverwrap is required in requirements.txt

--- hangupsbot/plugins/cleverbot.py
+++ hangupsbot/plugins/cleverbot.py
@@ -15,12 +15,6 @@ def _initialise(bot):
     plugins.register_handler(_handle_incoming_message, type="message")
     plugins.register_user_command(["chat"])
     plugins.register_admin_command(["chatreset"])
-    api_key = bot.get_config_option("cleverbot-api-key")
-    if not api_key:
-        logger.error("No cleverbot API key defined")
-    else:
-        global cw
-        cw = CleverWrap(api_key)
 
 @asyncio.coroutine
 def _handle_incoming_message(bot, event, command):
@@ -32,25 +26,35 @@ def _handle_incoming_message(bot, event, command):
     if not bot.get_config_suboption(event.conv_id, 'cleverbot_percentage_replies'):
         return
 
-    if not cw:
-        return
-
     percentage = bot.get_config_suboption(event.conv_id, 'cleverbot_percentage_replies')
 
     if randrange(0, 101, 1) < float(percentage):
-        text = cw.say(event.text)
-        if text:
-            yield from bot.coro_send_message(event.conv_id, text)
-
+        yield from chat(bot, event)
 
 def chat(bot, event, *args):
+    """ Chat to Cleverbot. Usage: /bot chat hi cleverbot! """
     if not cw:
-        yield from bot.coro_send_message(event.conv_id, "API key not defined")
+        api_key = bot.get_config_option("cleverbot_api_key")
+        if not api_key:
+            response = "API key not defined: config.cleverbot_api_key"
+            logger.error(response)
+            yield from bot.coro_send_message(event.conv_id, response)
+            return
+        else:
+            global cw
+            cw = CleverWrap(api_key)
+    if args:
+        input_text = " ".join(args)
     else:
-        text = cw.say(event.text)
-        yield from bot.coro_send_message(event.conv_id, text)
+        input_text = event.text
+    response = cw.say(input_text)
+    # cw.say takes one argument, the input string. It is a blocking call that returns cleverbot's response.
+    # see https://github.com/edwardslabs/cleverwrap.py for more information
+    yield from bot.coro_send_message(event.conv_id, response)
 
 
 def chatreset(bot, event, *args):
-    cw.reset()
+    """Tell cleverbot to forget things you've said in the past"""
+    if cw:
+        cw.reset()
     yield from bot.coro_send_message(event.conv_id, "reset")
