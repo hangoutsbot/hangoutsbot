c08b338649ba18aeb279fbbf0fd26dfafb6845b1
null pointer <eol+temporary-2@invalidfile.name>
2017-02-28 16:42:16 +0800

    gifv, webm on imgur are wrapped in a player, get raw file

--- hangupsbot/plugins/image_linker_reddit/__init__.py
+++ hangupsbot/plugins/image_linker_reddit/__init__.py
@@ -48,6 +48,11 @@ def _scan_for_triggers(bot, event, command):
                 r = yield from aiohttp.request('get', image_link)
                 raw = yield from r.read()
                 image_link = re.search("href=\"(.*?)\">GIF</a>", str(raw, 'utf-8')).group(1)
+
+            if "imgur.com" in image_link:
+                image_link = image_link.replace(".gifv",".gif")
+                image_link = image_link.replace(".webm",".gif")
+
             filename = os.path.basename(image_link)
             r = yield from aiohttp.request('get', image_link)
             raw = yield from r.read()
