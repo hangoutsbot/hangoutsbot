7336a486676878bfdb693339090d3f1fbd0d98b7
Nick Young <nbay92@gmail.com>
2017-03-10 22:14:33 +1300

    fix for V3

--- hangupsbot/plugins/album.py
+++ hangupsbot/plugins/album.py
@@ -8,8 +8,7 @@ def _check_for_image(bot, event, command):
     try:
         a = event.conv_event._event.chat_message.message_content.attachment[0]
         e = a.embed_item
-        d = e.data or e.data_
-        album = d['27639957'][0][0]
+        album = e.plus_photo.thumbnail.url
         album = album[0:album.index('?')]
         bot.conversation_memory_set(event.conv_id, 'album', album)
     except:
