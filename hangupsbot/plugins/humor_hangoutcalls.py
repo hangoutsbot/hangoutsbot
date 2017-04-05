1ecb52cde6ec717f9746d370359aae5e2abbb3a9
bing0USMC <binghamjd@gmail.com>
2017-04-05 00:59:26 -0400

    Update humor_hangoutcalls.py

--- hangupsbot/plugins/humor_hangoutcalls.py
+++ hangupsbot/plugins/humor_hangoutcalls.py
@@ -34,12 +34,12 @@ def on_hangout_call(bot, event, command):
             else:
                 """subsequent calls for a GROUP"""
                 yield from bot.coro_send_message(event.conv_id,
-                    _("<b>It's been {} since the last call. The last caller was <i>{}</i>. <br><br> To prevent accidental calls, remove the microphone permission from the hangout app.</b>").format(humantime, lastcaller))
+                    _("<b>It's been {} since the last call. The last caller was <i>{}</i>. <br><br> To prevent accidental calls, remove the microphone permission from the hangouts app.</b>").format(humantime, lastcaller))
 
         else:
             """first ever call for any conversation"""
             yield from bot.coro_send_message(event.conv_id,
-                _("<b>No prizes for that call <br><br> To prevent accidental calls, remove the microphone permission from the hangout app.</b>"))
+                _("<b>No prizes for that call <br><br> To prevent accidental calls, remove the microphone permission from the hangouts app.</b>"))
 
         bot.conversation_memory_set(event.conv_id, "lastcall", { "caller": event.user.full_name, "timestamp": time.time() })
 
