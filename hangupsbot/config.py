eb5e9c9a933b1ee89f8d045ddadf5548533ad48c
das7pad <das7pad@outlook.com>
2017-04-18 01:48:33 +0200

    add detailed docstrings

--- hangupsbot/config.py
+++ hangupsbot/config.py
@@ -1,3 +1,5 @@
+"""cache for a json file with shortscuts to access data"""
+
 import asyncio
 import collections
 from datetime import datetime
@@ -14,7 +16,14 @@ logger = logging.getLogger(__name__)
 
 
 class Config(collections.MutableMapping):
-    """Configuration JSON storage class"""
+    """Configuration JSON storage class
+
+    Args:
+        path: string, file path of the config file
+        default: any type, default value for missing data
+        failsafe_backups: int, ammount of backups that should be kept
+        save_delay: int, time in second a dump should be delayed
+    """
     def __init__(self, path, default=None, failsafe_backups=0, save_delay=0):
         self.filename = path
         self.default = default
@@ -42,6 +51,13 @@ class Config(collections.MutableMapping):
             return True
 
     def _make_failsafe_backup(self):
+        """remove old backup files above the limit and create a new backup
+
+        the limit refers to the number of .failsafe_backups
+
+        Returns:
+            boolean, True on a successful new backup, otherwise False
+        """
         try:
             with open(self.filename) as file:
                 json.load(file)
@@ -65,6 +81,12 @@ class Config(collections.MutableMapping):
         return True
 
     def _recover_from_failsafe(self):
+        """restore data from a recent backup
+
+        Returns:
+            boolean, True if any backup could be loaded, False if None is
+                available or all backups are currupt or no readable
+        """
         existing = sorted(glob.glob(self.filename + ".*.bak"))
         recovery_filename = None
         while len(existing) > 0:
@@ -85,6 +107,14 @@ class Config(collections.MutableMapping):
         return False
 
     def load(self):
+        """Load config from file
+
+        Raises:
+            IOError: the existing config is not readable or no new config can
+                be saved to the configured path
+            ValueError: the config file is not a valid json and no backups are
+                available
+        """
         try:
             with open(self.filename) as file:
                 data = file.read()
@@ -150,10 +180,24 @@ class Config(collections.MutableMapping):
         logger.info("%s write %s", self.filename, interval)
 
     def flush(self):
+        """force an immediate dump to file"""
         logger.info("flushing %s", self.filename)
         self.save(delay=False)
 
     def get_by_path(self, keys_list, fallback=True):
+        """Get an item from .config by path
+
+        Args:
+            keys_list: list, a list of strings, describing the path to the value
+            fallback: boolean, use the default values as fallback for missing
+                entrys
+
+        Returns:
+            any type, the requested value
+
+        Raises:
+            KeyError, ValueError: the path does not exist
+        """
         try:
             return self._get_by_path(self.config, keys_list)
         except (KeyError, ValueError):
@@ -166,34 +210,95 @@ class Config(collections.MutableMapping):
                                (self.filename, keys_list))
 
     def set_by_path(self, keys_list, value, create_path=True):
+        """set an item in .config by path
+
+        Args:
+            keys_list: list, a list of strings, describing the path to the value
+            value: any type, the new value
+            create_path: boolean, toogle to ensure an existing path
+
+        Raises:
+            KeyError, ValueError: the path does not exist
+        """
         if create_path:
             self.ensure_path(keys_list)
         self.get_by_path(keys_list[:-1],
                          fallback=False)[keys_list[-1]] = value
 
     def pop_by_path(self, keys_list):
+        """remove an item in .config found with the given path
+
+        Args:
+            keys_list: list, a list of strings, describing the path to the value
+
+        Returns:
+            any type, the removed value
+
+        Raises:
+            KeyError, ValueError: the path does not exist
+        """
         return self.get_by_path(keys_list[:-1], False).pop(keys_list[-1])
 
     @staticmethod
     def _get_by_path(source, path):
+        """Get an item from source by path
+
+        Args:
+            path: list, a list of strings, describing the path to the value
+
+        Returns:
+            any type, the requested value
+
+        Raises:
+            KeyError, ValueError: the path does not exist
+        """
         if not len(path):
             return source
         return functools.reduce(operator.getitem, path[:-1], source)[path[-1]]
 
     def get_option(self, keyname):
+        """get a top level entry from config or a default value
+
+        Args:
+            keyname: string, top level key
+
+        Returns:
+            any type, the requested value or .default if the key does not exist
+        """
         try:
             return self.get_by_path([keyname])
         except KeyError:
             return self.default
 
     def get_suboption(self, grouping, groupname, keyname):
+        """get a third level entry from config with a fallback to top level
+
+        Args:
+            grouping: string, top level entry in .config
+            groupname: string, second level entry, key in grouping
+            keyname: string, third level key as target and also the top level
+                key as fallback for a missing key in the path
+
+        Returns:
+            any type, the requested value, it's fallback on top level or
+                .default if the key does not exist on both level
+        """
         try:
             return self.get_by_path([grouping, groupname, keyname])
         except KeyError:
             return self.get_option(keyname)
 
     def exists(self, keys_list, fallback=False):
+        """check if a path exisits in the dict
 
+        Args:
+            keys_list: list, a list of strings describing the path
+            fallback: boolean, use the default values as fallback for missing
+                entrys
+
+        Returns:
+            boolean, True if the full path is resolvable, otherwise False
+        """
         try:
             self.get_by_path(keys_list, fallback)
             return True
@@ -201,6 +306,19 @@ class Config(collections.MutableMapping):
             return False
 
     def ensure_path(self, path, base=None):
+        """create a path of dicts if the given path does not exist
+
+        Args:
+            path: list, a list of strings describing the path
+            base: dict, the source to create the path in
+
+        Returns:
+            boolean, True if the path did not existed before, otherwise False
+
+        Raises:
+            AttributeError: on attempting to override a key pointing to an entry
+                in config that is not a dict
+        """
         if base is None:
             base = self.config
         created = not self.exists(path)
@@ -215,6 +333,20 @@ class Config(collections.MutableMapping):
         return created
 
     def set_defaults(self, source, path=None):
+        """ensure that the dict, path points to, has the structure of the source
+
+        also override to defaults if the type of an entry does not match
+
+        Args:
+            source: dict structure with values
+            path: list, a list of keys to the dict to update with defaults
+
+        Raises:
+            ValueError: the path could not be created as it would override an
+                entry that is not a dict
+            AttributeError: a value in source does not match with the type that
+                is already in the defaults
+        """
         if path is None:
             path = []
         else:
@@ -231,6 +363,18 @@ class Config(collections.MutableMapping):
                 defaults[key].extend(value)
 
     def validate(self, source, path=None):
+        """ensure that the entrys in source are all available in the config
+
+        also override to defaults if the type of an entry does not match
+
+        Args:
+            source: default dict structure with values
+            path: list of keys to the dict to validate
+
+        Raises:
+            ValueError: the path could not be created as it would override an
+                entry that is not a dict
+        """
         if path is None:
             path = []
         for key, value in source.items():
