12df1477e0171191c98ac7ec761db77ef083ce63
Evan Gubarev <evan@evangubarev.com>
2020-03-24 16:14:58 -0700

    Create covid19.py

---
+++ hangupsbot/plugins/covid19.py
@@ -0,0 +1,21 @@
+import logging
+import plugins
+import requests
+
+logger = logging.getLogger(__name__)
+
+def _initialise(bot):
+    plugins.register_user_command(["covid19"])
+    
+def covid19(bot, event, *args):
+    """reply to a ping"""
+    try:   
+        page = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+        pagearray = json.loads(page.text)
+        cases = str(pagearray["cases"])
+        deaths = str(pagearray["deaths"])
+        html_text = "The COVID-19 outbreak has " + cases + " cases and " + deaths + "$
+    except:
+        html_text = "Unable to get catfacts right now"
+        logger.exception(html_text)
+    yield from bot.coro_send_message(event.conv_id, html_text)
