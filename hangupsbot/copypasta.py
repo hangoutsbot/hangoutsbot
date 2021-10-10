58865701f81281d6f9170fcd39b7b7a055b13794
Connor Ahern <connormahern25@gmail.com>
2021-10-10 08:56:30 -0700

    Create a copypasta command

---
+++ hangupsbot/copypasta.py
@@ -0,0 +1,21 @@
+import time
+import plugins
+import requests
+import hangups
+import logging
+import random
+
+logger = logging.getLogger(__name__)
+
+def _initialise(bot):
+    plugins.register_user_command(["copypasta"])
+
+def copypasta(bot, event, *args):
+    try:
+        r = requests.get("https://www.reddit.com/r/copypasta/new/.json?sort=top&t=week&showmedia=false&mediaonly=false&is_self=true&limit=100", headers = {'User-agent': 'your bot 0.1'})
+        pastas = r.json()['data']['children'][random.randint(0, 99)]['data']['selftext']
+    except:
+        html_text = "Unable to get copypastas right now"
+        logger.exception(pastas)
+
+    yield from bot.coro_send_message(event.conv_id, pastas)
